tarantool/tarantool

commit 1376aad9b3fd1fa3c42a220dafeadaa63387f7a2

Refactor src/uri.rl to support RFC3986 and add Lua bindings
 * Split parser and resolver in src/uri.rl
 * Extend URI parser grammar to fully support RFC3986
 * Add Lua bindings to URI parser
 * Implement proper DNS logic in coio_connect() /
   evio_service_bind()
 * Replace uri_to_string() with sio_strfaddr()

Prerequisite for #447

src/coio.cc
@@ -28,12 +28,15 @@
  */
 #include "coio.h"
 
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <netinet/tcp.h>
 #include <stdio.h>
 
 #include "iobuf.h"
 #include "sio.h"
 #include "scoped_guard.h"
+#include "coeio.h" /* coeio_resolve() */
 
 struct CoioGuard {
 	struct ev_io *ev_io;
@@ -79,27 +82,22 @@ coio_fiber_yield_timeout(struct ev_io *coio, ev_tstamp delay)
 }
 
 /**
- * Connect to a host.
- */
-void
-coio_connect(struct ev_io *coio, struct sockaddr *addr, socklen_t addr_len)
-{
-	coio_connect_timeout(coio, addr, addr_len, TIMEOUT_INFINITY);
-}
-
-/**
  * Connect to a host with a specified timeout.
- * @retval true timeout
- * @retval false connected
+ * @retval -1 timeout
+ * @retval 0 connected
  */
-bool
-coio_connect_timeout(struct ev_io *coio, struct sockaddr *addr,
-		     socklen_t len, ev_tstamp timeout)
+static int
+coio_connect_addr(struct ev_io *coio, struct sockaddr *addr,
+		  socklen_t len, ev_tstamp timeout)
 {
-	if (sio_connect(coio->fd, addr, len) == 0)
-		return false;
-	assert(errno == EINPROGRESS);
 	ev_loop *loop = loop();
+	evio_socket(coio, addr->sa_family, SOCK_STREAM, 0);
+	auto coio_guard = make_scoped_guard([=]{ evio_close(loop, coio); });
+	if (sio_connect(coio->fd, addr, len) == 0) {
+		coio_guard.is_active = false;
+		return 0;
+	}
+	assert(errno == EINPROGRESS);
 	/*
 	 * Wait until socket is ready for writing or
 	 * timed out.
@@ -111,7 +109,7 @@ coio_connect_timeout(struct ev_io *coio, struct sockaddr *addr,
 	fiber_testcancel();
 	if (is_timedout) {
 		errno = ETIMEDOUT;
-		return true;
+		return -1;
 	}
 	int error = EINPROGRESS;
 	socklen_t sz = sizeof(error);
@@ -121,50 +119,87 @@ coio_connect_timeout(struct ev_io *coio, struct sockaddr *addr,
 		errno = error;
 		tnt_raise(SocketError, coio->fd, "connect");
 	}
-	return false;
+	coio_guard.is_active = false;
+	return 0;
 }
 
 /**
- * Connect to a first address in addrinfo list and initialize coio
- * with connected socket.
+ * Resolve hostname:service from \a uri and connect to the first available
+ * address with a specified timeout.
  *
- * If coio is already initialized, socket family,
- * type and protocol must match the remote address.
+ * If \a addr is not NULL the function provides resolved address on success.
+ * In this case, \a addr_len is a value-result argument. It should be
+ * initialized to the size of the buffer associated with \a addr. Upon return,
+ * \a addr_len is updated to contain the actual size of the source address.
+ * The returned address is truncated if the buffer provided is too small;
+ * in this case, addrlen will return a value greater than was supplied to the
+ * call.
  *
- * @retval true  timeout
- * @retval false sucess
+ * This function also supports UNIX domain sockets if uri->path is not NULL and
+ * uri->service is NULL.
+ *
+ * @retval -1 timeout
+ * @retval 0 connected
  */
-bool
-coio_connect_addrinfo(struct ev_io *coio, struct addrinfo *ai,
-		      ev_tstamp timeout)
+int
+coio_connect_timeout(struct ev_io *coio, const char *host, const char *service,
+		     struct sockaddr *addr, socklen_t *addr_len,
+		     ev_tstamp timeout)
 {
+	/* try to resolve a hostname */
+	struct ev_loop *loop = loop();
 	ev_tstamp start, delay;
-	ev_loop *loop = loop();
+	evio_timeout_init(loop, &start, &delay, timeout);
+
+	assert(service != NULL);
+	if (strcmp(host, URI_HOST_UNIX) == 0) {
+		/* UNIX socket */
+		struct sockaddr_un un;
+		snprintf(un.sun_path, sizeof(un.sun_path), "%s", service);
+		un.sun_family = AF_UNIX;
+		if (coio_connect_addr(coio, (struct sockaddr *) &un, sizeof(un),
+				      delay) != 0)
+			return -1;
+		if (addr != NULL) {
+			assert(addr_len != NULL);
+			*addr_len = MIN(sizeof(un), *addr_len);
+			memcpy(addr, &un, *addr_len);
+		}
+		return 0;
+	}
+
+	struct addrinfo *ai = coeio_resolve(SOCK_STREAM, host, service, delay);
+	if (ai == NULL)
+		return -1; /* timeout */
+
+	auto addrinfo_guard = make_scoped_guard([=]{ freeaddrinfo(ai); });
+	evio_timeout_update(loop(), start, &delay);
+
 	coio_timeout_init(&start, &delay, timeout);
 	assert(! evio_is_active(coio));
-	bool res = true;
 	while (ai) {
 		try {
-			evio_socket(coio, ai->ai_family,
-				    ai->ai_socktype,
-				    ai->ai_protocol);
-			res = coio_connect_timeout(coio, ai->ai_addr,
-					ai->ai_addrlen, delay);
-			if (res)
-				evio_close(loop, coio);
-			return res;
+			if (coio_connect_addr(coio, ai->ai_addr,
+					      ai->ai_addrlen, delay))
+				return -1;
+			if (addr != NULL) {
+				assert(addr_len != NULL);
+				*addr_len = MIN(ai->ai_addrlen, *addr_len);
+				memcpy(addr, ai->ai_addr, *addr_len);
+			}
+			return 0; /* connected */
 		} catch (SocketError *e) {
-			if (res)
-				evio_close(loop, coio);
-			if (ai->ai_next == NULL)
-				throw;
-			ev_now_update(loop);
-			coio_timeout_update(start, &delay);
+			/* ignore */
+			say_error("failed to connect to %s: %s",
+				  sio_strfaddr(ai->ai_addr, ai->ai_addrlen),
+				  e->errmsg());
 		}
 		ai = ai->ai_next;
+		ev_now_update(loop);
+		coio_timeout_update(start, &delay);
 	}
-	/* unreachable. */
-	tnt_raise(SocketError, coio->fd, "connect_addrinfo()");
+
+	tnt_raise(SocketError, coio->fd, "connection failed");
 }
 
 /**

src/CMakeLists.txt
@@ -20,6 +20,7 @@ lua_source(lua_sources lua/init.lua)
 lua_source(lua_sources lua/uuid.lua)
 lua_source(lua_sources lua/digest.lua)
 lua_source(lua_sources lua/msgpackffi.lua)
+lua_source(lua_sources lua/uri.lua)
 lua_source(lua_sources lua/console.lua)
 lua_source(lua_sources lua/bsdsocket.lua)
 lua_source(lua_sources lua/errno.lua)
@@ -39,7 +40,7 @@ set_property(DIRECTORY PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${lua_sources})
 
 add_custom_target(ragel
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    COMMAND ragel -G2 src/uri.rl -o src/uri.cc)
+    COMMAND ragel -G2 src/uri.rl -o src/uri.c)
 
 set (common_sources
      memory.cc
@@ -73,7 +74,7 @@ set (common_sources
      fiob.c
      tt_uuid.c
      ffisyms.cc
-     uri.cc
+     uri.c
      coeio_file.cc
      lua/digest.cc
      lua/init.cc

src/box/box.cc
@@ -114,9 +114,11 @@ box_check_replication_source(const char *source)
 	if (source == NULL)
 		return;
 	struct uri uri;
-	if (uri_parse(&uri, source)) {
-		tnt_raise(ClientError, ER_CFG,
-			  "incorrect replication source");
+
+	/* URI format is [host:]service */
+	if (uri_parse(&uri, source) || !uri.service) {
+		tnt_raise(ClientError, ER_CFG, "replication source, "
+			  "expected host:service or /unix.socket");
 	}
 }
 
@@ -134,6 +136,7 @@ static void
 box_check_config()
 {
 	box_check_wal_mode(cfg_gets("wal_mode"));
+	box_check_replication_source(cfg_gets("replication_source"));
 
 	/* check rows_per_wal configuration */
 	if (cfg_geti("rows_per_wal") <= 1) {

src/box/replica.cc
@@ -89,21 +89,43 @@ remote_connect(struct recovery_state *r, struct ev_io *coio,
 {
 	char greeting[IPROTO_GREETING_SIZE];
 
-	evio_socket(coio, AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
+	struct remote *remote = &r->remote;
 	struct uri *uri = &r->remote.uri;
-
-	coio_connect(coio, &uri->addr, uri->addr_len);
+	/*
+	 * coio_connect() stores resolved address to \a &remote->addr
+	 * on success. &remote->addr_len is a value-result argument which
+	 * must be initialized to the size of associated buffer (addrstorage)
+	 * before calling coio_connect(). Since coio_connect() performs
+	 * DNS resolution under the hood it is theoretically possible that
+	 * remote->addr_len will be different even for same uri.
+	 */
+	remote->addr_len = sizeof(remote->addrstorage);
+	/* Prepare null-terminated strings for coio_connect() */
+	char host[URI_MAXHOST] = { '\0' };
+	if (uri->host) {
+		snprintf(host, sizeof(host), "%.*s", (int) uri->host_len,
+			 uri->host);
+	}
+	char service[URI_MAXSERVICE];
+	snprintf(service, sizeof(service), "%.*s", (int) uri->service_len,
+		 uri->service);
+	coio_connect(coio, host, service, &remote->addr, &remote->addr_len);
+	assert(coio->fd >= 0);
 	coio_readn(coio, greeting, sizeof(greeting));
 
-	say_crit("connected to master");
-	if (!r->remote.uri.login[0])
+	say_crit("connected to %s", sio_strfaddr(&remote->addr,
+						 remote->addr_len));
+
+	/* Perform authentication if user provided at least login */
+	if (!r->remote.uri.login)
 		return;
 
 	/* Authenticate */
 	say_debug("authenticating...");
 	struct xrow_header row;
-	xrow_encode_auth(&row, greeting, uri->login, uri->password);
+	xrow_encode_auth(&row, greeting, uri->login,
+			 uri->login_len, uri->password,
+			 uri->password_len);
 	remote_write_row(coio, &row);
 	remote_read_row(coio, iobuf, &row);
 	if (row.type != IPROTO_OK)
@@ -285,7 +307,7 @@ recovery_follow_remote(struct recovery_state *r)
 	assert(r->remote.reader == NULL);
 	assert(recovery_has_remote(r));
 
-	const char *uri = uri_to_string(&r->remote.uri);
+	const char *uri = uri_format(&r->remote.uri);
 	say_crit("starting replication from %s", uri);
 	snprintf(name, sizeof(name), "replica/%s", uri);
 
@@ -316,14 +338,12 @@ recovery_set_remote(struct recovery_state *r, const char *uri)
 		r->remote.source[0] = '\0';
 		return;
 	}
-	/*
-	 * @todo: as long as DNS is involved, this may fail even
-	 * on a valid uri. Don't panic in this case.
-	 */
-	if (uri_parse(&r->remote.uri, uri))
-		panic("Can't parse uri: %s", uri);
-	snprintf(r->remote.source,
-		 sizeof(r->remote.source), "%s", uri);
+	snprintf(r->remote.source, sizeof(r->remote.source), "%s", uri);
+	struct remote *remote = &r->remote;
+	int rc = uri_parse(&remote->uri, r->remote.source);
+	/* URI checked by box_check_replication_source() */
+	assert(rc == 0 && remote->uri.service != NULL);
+	(void) rc;
 }
 
 bool

src/box/replica.h
@@ -32,15 +32,20 @@
 #include "tarantool_ev.h"
 #include <uri.h>
 
-enum { REMOTE_SOURCE_MAXLEN = 64 };
+enum { REMOTE_SOURCE_MAXLEN = 1024 }; /* enough to fit URI with passwords */
 
 /** Master connection */
 struct remote {
-	struct uri uri;
 	struct fiber *reader;
 	ev_tstamp recovery_lag, recovery_last_update_tstamp;
 	bool warning_said;
 	char source[REMOTE_SOURCE_MAXLEN];
+	struct uri uri;
+	union {
+		struct sockaddr addr;
+		struct sockaddr_storage addrstorage;
+	};
+	socklen_t addr_len;
 };
 
 /** Connect to a master and request a snapshot.

src/box/xrow.cc
@@ -185,32 +185,32 @@ xrow_to_iovec(const struct xrow_header *row,
 
 void
 xrow_encode_auth(struct xrow_header *packet, const char *greeting,
-		   const char *login, const char *password)
+		 const char *login, size_t login_len,
+		 const char *password, size_t password_len)
 {
+	assert(login != NULL);
 	memset(packet, 0, sizeof(*packet));
-
-	uint32_t login_len = strlen(login);
-	uint32_t password_len = strlen(password);
 
 	enum { PACKET_LEN_MAX = 128 };
 	size_t buf_size = PACKET_LEN_MAX + login_len + SCRAMBLE_SIZE;
 	char *buf = (char *) region_alloc(&fiber()->gc, buf_size);
 
-	char salt[SCRAMBLE_SIZE];
-	char scramble[SCRAMBLE_SIZE];
-	if (base64_decode(greeting + 64, SCRAMBLE_BASE64_SIZE, salt,
-			  SCRAMBLE_SIZE) != SCRAMBLE_SIZE)
-		panic("invalid salt: %64s", greeting + 64);
-	scramble_prepare(scramble, salt, password, password_len);
-
 	char *d = buf;
-	d = mp_encode_map(d, 2);
+	d = mp_encode_map(d, password != NULL ? 2 : 1);
 	d = mp_encode_uint(d, IPROTO_USER_NAME);
 	d = mp_encode_str(d, login, login_len);
-	d = mp_encode_uint(d, IPROTO_TUPLE);
-	d = mp_encode_array(d, 2);
-	d = mp_encode_str(d, "chap-sha1", strlen("chap-sha1"));
-	d = mp_encode_str(d, scramble, SCRAMBLE_SIZE);
+	if (password != NULL) { /* password can be omitted */
+		char salt[SCRAMBLE_SIZE];
+		char scramble[SCRAMBLE_SIZE];
+		if (base64_decode(greeting + 64, SCRAMBLE_BASE64_SIZE, salt,
+				  SCRAMBLE_SIZE) != SCRAMBLE_SIZE)
+			panic("invalid salt: %64s", greeting + 64);
+		scramble_prepare(scramble, salt, password, password_len);
+		d = mp_encode_uint(d, IPROTO_TUPLE);
+		d = mp_encode_array(d, 2);
+		d = mp_encode_str(d, "chap-sha1", strlen("chap-sha1"));
+		d = mp_encode_str(d, scramble, SCRAMBLE_SIZE);
+	}
 
 	assert(d <= buf + buf_size);
 	packet->body[0].iov_base = buf;

src/box/xrow.h
@@ -83,11 +83,14 @@ xrow_decode_error(struct xrow_header *row);
  * \param[out] row
  * \param greeting - IPROTO greeting
  * \param login - user login
+ * \param login_len - length of \a login
  * \param password - user password
+ * \param password_len - length of \a password
 */
 void
 xrow_encode_auth(struct xrow_header *row, const char *greeting,
-		 const char *login, const char *password);
+		 const char *login, size_t login_len,
+		 const char *password, size_t password_len);
 
 /**
  * \brief Encode SUBSCRIBE command

src/coeio.cc
@@ -260,8 +260,9 @@ coeio_resolve(int socktype, const char *host, const char *port,
 	hints.ai_protocol = 0;
 	/* do resolving */
 	errno = 0;
-	if (coeio_custom(getaddrinfo_cb, timeout, host, port,
-			 &hints, &result))
+	/* make no difference between empty string and NULL for host */
+	if (coeio_custom(getaddrinfo_cb, timeout, (host && *host) ? host : NULL,
+			 port, &hints, &result))
 		return NULL;
 	return result;
 }

tsafin/tarantool-dsql

commit c84f648b8d871f1bf99b405893c09b13710bfcb0

Fix #582: Replication: repetitive message about failing to connect
Regression introduced by 1376aad9b3fd1fa3c42a220dafeadaa63387f7a2

src/coio.cc
@@ -191,10 +191,9 @@ coio_connect_timeout(struct ev_io *coio, const char *host, const char *service,
 			}
 			return 0; /* connected */
 		} catch (SocketError *e) {
-			/* ignore */
-			say_error("failed to connect to %s: %s",
-				  sio_strfaddr(ai->ai_addr, ai->ai_addrlen),
-				  e->errmsg());
+			if (ai->ai_next == NULL)
+				throw;
+			/* ignore exception and try the next address */
 		}
 		ai = ai->ai_next;
 		ev_now_update(loop);