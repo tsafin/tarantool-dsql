tarantool/tarantool

commit 0b17f366a0aae82bd63791c76b9b3b887588f392

module api docs: remove double title, fix spelling

src/trivia/tarantool_header.h
@@ -8,13 +8,7 @@
 #include <stdint.h>
 #include <sys/types.h> /* ssize_t */
 
-
-/**
- * \file
- * Tarantool Module API
- */
-
-/** Extern modificator for all public functions */
+/** Extern modifier for all public functions */
 #if defined(__cplusplus)
 #define API_EXPORT extern "C" __attribute__ ((visibility ("default")))
 #else

tsafin/tarantool-dsql

commit 7033ddc0493df3dbca9f3f938a25ab92108ed760

Fix #1020: Build warnings when building public C API manual
Regression by 0b17f366a0aae82bd63791c76b9b3b887588f392

Doxyfile
@@ -319,22 +319,6 @@ INLINE_SIMPLE_STRUCTS  = NO
 
 TYPEDEF_HIDES_STRUCT   = NO
 
-# The SYMBOL_CACHE_SIZE determines the size of the internal cache use to
-# determine which symbols to keep in memory and which to flush to disk.
-# When the cache is full, less often used symbols will be written to disk.
-# For small to medium size projects (<1000 input files) the default value is
-# probably good enough. For larger projects a too small cache size can cause
-# doxygen to be busy swapping symbols to and from disk most of the time
-# causing a significant performance penalty.
-# If the system has enough physical memory increasing the cache will improve the
-# performance by keeping more symbols in memory. Note that the value works on
-# a logarithmic scale so increasing the size by one will roughly double the
-# memory usage. The cache size is given by this formula:
-# 2^(16+SYMBOL_CACHE_SIZE). The valid range is 0..9, the default is 0,
-# corresponding to a cache size of 2^16 = 65536 symbols.
-
-SYMBOL_CACHE_SIZE      = 0
-
 # Similar to the SYMBOL_CACHE_SIZE the size of the symbol lookup cache can be
 # set using LOOKUP_CACHE_SIZE. This cache is used to resolve symbols given
 # their name and scope. Since this can be an expensive process and often the
@@ -1390,21 +1374,16 @@ GENERATE_XML           = YES
 
 # The XML_OUTPUT tag is used to specify where the XML pages will be put.
 # If a relative path is entered the value of OUTPUT_DIRECTORY will be
-# put in front of it. If left blank `xml' will be used as the default path.
+# put in front of it. If left blank `xml' will be used as thech can be used by a validating XML parser to check the
+# # syntax of the XML files.
+#
+# default path.
 
 XML_OUTPUT             = xml
 
 # The XML_SCHEMA tag can be used to specify an XML schema,
 # which can be used by a validating XML parser to check the
 # syntax of the XML files.
-
-XML_SCHEMA             =
-
-# The XML_DTD tag can be used to specify an XML DTD,
-# which can be used by a validating XML parser to check the
-# syntax of the XML files.
-
-XML_DTD                =
 
 # If the XML_PROGRAMLISTING tag is set to YES Doxygen will
 # dump the program listings (including syntax highlighting

src/trivia/tarantool_header.h
@@ -1,6 +1,10 @@
 #ifndef TARANTOOL_MODULE_H_INCLUDED
 #define TARANTOOL_MODULE_H_INCLUDED
 
+/**
+ * \file
+ */
+
 #include <stddef.h>
 #include <stdarg.h> /* va_list */
 #include <errno.h>