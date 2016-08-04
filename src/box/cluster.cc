wiltonlazary/tarantool

Compare UUIDs consistently
The C code uses memcmp() to compare UUIDs for server selection
in replication bootstrap, but in lua there is no way to do it,
because UUIDs there are represented as strings.

This patch fixes the C implementation so that it will behave
as if UUIDs are compared as strings.

Fixes #1598

src/box/cluster.cc
@@ -59,7 +59,7 @@ rb_proto(, serverset_, serverset_t, struct server)
 static int
 server_compare_by_uuid(const struct server *a, const struct server *b)
 {
-	return memcmp(&a->uuid, &b->uuid, sizeof(a->uuid));
+	return tt_uuid_cmp(&a->uuid, &b->uuid);
 }
 
 rb_gen(, serverset_, serverset_t, struct server, link,

src/tt_uuid.c
@@ -67,6 +67,9 @@ tt_uuid_create(struct tt_uuid *uu)
 extern inline int
 tt_uuid_from_string(const char *in, struct tt_uuid *uu);
 
+extern inline int
+tt_uuid_cmp(const struct tt_uuid *a, const struct tt_uuid *b);
+
 extern inline void
 tt_uuid_to_string(const struct tt_uuid *uu, char *out);
 

src/tt_uuid.h
@@ -87,6 +87,36 @@ tt_uuid_from_string(const char *in, struct tt_uuid *uu)
 }
 
 /**
+ * \brief Compare UUIDs lexicographically.
+ * \param a UUID
+ * \param b UUID
+ * \retval comparison result, as in strcmp()
+ */
+inline int
+tt_uuid_cmp(const struct tt_uuid *a, const struct tt_uuid *b)
+{
+#define cmp_tt_uuid_field(field)                \
+        if (a->field > b->field) return 1;      \
+        if (a->field < b->field) return -1;
+
+        cmp_tt_uuid_field(time_low);
+        cmp_tt_uuid_field(time_mid);
+        cmp_tt_uuid_field(time_hi_and_version);
+        cmp_tt_uuid_field(clock_seq_hi_and_reserved);
+        cmp_tt_uuid_field(clock_seq_low);
+        cmp_tt_uuid_field(node[0]);
+        cmp_tt_uuid_field(node[1]);
+        cmp_tt_uuid_field(node[2]);
+        cmp_tt_uuid_field(node[3]);
+        cmp_tt_uuid_field(node[4]);
+        cmp_tt_uuid_field(node[5]);
+
+#undef cmp_tt_uuid_field
+
+        return 0;
+}
+
+/**
  * \brief Format UUID to RFC 4122 string.
  * \param uu uuid
  * \param[out] out buffer, must be at least UUID_STR_LEN + 1 length

test/unit/uuid.c
@@ -1,0 +1,67 @@
+#include "unit.h"
+#include <tt_uuid.h>
+#include <string.h>
+
+static void
+uuid_test(struct tt_uuid a, struct tt_uuid b, int expected_result)
+{
+        char a_str[UUID_STR_LEN];
+        char b_str[UUID_STR_LEN];
+
+        tt_uuid_to_string(&a, a_str);
+        tt_uuid_to_string(&b, b_str);
+
+        int cmp_result = tt_uuid_cmp(&a, &b);
+
+        char *sign = 0;
+
+        if (cmp_result == 1)
+                sign = ">";
+        else if (cmp_result == -1)
+                sign = "<";
+        else
+                sign = "=";
+
+        is(cmp_result,
+           expected_result,
+           "%s %s %s", a_str, sign, b_str);
+}
+
+int
+main(void)
+{
+        plan(2);
+
+        uuid_test(
+                (struct tt_uuid){.time_low = 1712399963,
+                                .time_mid = 34898,
+                                .time_hi_and_version = 18482,
+                                .clock_seq_hi_and_reserved = 175,
+                                .clock_seq_low = 139,
+                                .node = "Ad\325,b\353"},
+                (struct tt_uuid){.time_low = 409910263,
+                                .time_mid = 53143,
+                                .time_hi_and_version = 20014,
+                                .clock_seq_hi_and_reserved = 139,
+                                .clock_seq_low = 27,
+                                .node = "v\025Oo9I"},
+                1);
+
+
+        uuid_test(
+                (struct tt_uuid){.time_low = 123421000,
+                                .time_mid = 36784,
+                                .time_hi_and_version = 11903,
+                                .clock_seq_hi_and_reserved = 175,
+                                .clock_seq_low = 80,
+                                .node = "Ad\325,b\353"},
+                (struct tt_uuid){.time_low = 532451999,
+                                .time_mid = 23976,
+                                .time_hi_and_version = 10437,
+                                .clock_seq_hi_and_reserved = 139,
+                                .clock_seq_low = 54,
+                                .node = "v\025Oo9I"},
+                -1);
+
+        return check_plan();
+}

tsafin/tarantool-dsql

src/box/cluster.cc
@@ -59,7 +59,7 @@ rb_proto(, serverset_, serverset_t, struct server)
 static int
 server_compare_by_uuid(const struct server *a, const struct server *b)
 {
-	return memcmp(&a->uuid, &b->uuid, sizeof(a->uuid));
+	return tt_uuid_compare(&a->uuid, &b->uuid);
 }
 
 rb_gen(, serverset_, serverset_t, struct server, link,

src/tt_uuid.c
@@ -67,6 +67,9 @@ tt_uuid_create(struct tt_uuid *uu)
 extern inline int
 tt_uuid_from_string(const char *in, struct tt_uuid *uu);
 
+extern inline int
+tt_uuid_compare(const struct tt_uuid *a, const struct tt_uuid *b);
+
 extern inline void
 tt_uuid_to_string(const struct tt_uuid *uu, char *out);
 

src/tt_uuid.h
@@ -87,6 +87,36 @@ tt_uuid_from_string(const char *in, struct tt_uuid *uu)
 }
 
 /**
+ * \brief Compare UUIDs lexicographically.
+ * \param a UUID
+ * \param b UUID
+ * \retval comparison result, as in strcmp()
+ */
+inline int
+tt_uuid_compare(const struct tt_uuid *a, const struct tt_uuid *b)
+{
+#define cmp_tt_uuid_field(field)                \
+        if (a->field > b->field) return 1;      \
+        if (a->field < b->field) return -1;
+
+        cmp_tt_uuid_field(time_low);
+        cmp_tt_uuid_field(time_mid);
+        cmp_tt_uuid_field(time_hi_and_version);
+        cmp_tt_uuid_field(clock_seq_hi_and_reserved);
+        cmp_tt_uuid_field(clock_seq_low);
+        cmp_tt_uuid_field(node[0]);
+        cmp_tt_uuid_field(node[1]);
+        cmp_tt_uuid_field(node[2]);
+        cmp_tt_uuid_field(node[3]);
+        cmp_tt_uuid_field(node[4]);
+        cmp_tt_uuid_field(node[5]);
+
+#undef cmp_tt_uuid_field
+
+        return 0;
+}
+
+/**
  * \brief Format UUID to RFC 4122 string.
  * \param uu uuid
  * \param[out] out buffer, must be at least UUID_STR_LEN + 1 length

test/unit/uuid.c
@@ -1,0 +1,67 @@
+#include "unit.h"
+#include <tt_uuid.h>
+#include <string.h>
+
+static void
+uuid_test(struct tt_uuid a, struct tt_uuid b, int expected_result)
+{
+        char a_str[UUID_STR_LEN];
+        char b_str[UUID_STR_LEN];
+
+        tt_uuid_to_string(&a, a_str);
+        tt_uuid_to_string(&b, b_str);
+
+        int cmp_result = tt_uuid_compare(&a, &b);
+
+        char *sign = 0;
+
+        if (cmp_result == 1)
+                sign = ">";
+        else if (cmp_result == -1)
+                sign = "<";
+        else
+                sign = "=";
+
+        is(cmp_result,
+           expected_result,
+           "%s %s %s", a_str, sign, b_str);
+}
+
+int
+main(void)
+{
+        plan(2);
+
+        uuid_test(
+                (struct tt_uuid){.time_low = 1712399963,
+                                .time_mid = 34898,
+                                .time_hi_and_version = 18482,
+                                .clock_seq_hi_and_reserved = 175,
+                                .clock_seq_low = 139,
+                                .node = "Ad\325,b\353"},
+                (struct tt_uuid){.time_low = 409910263,
+                                .time_mid = 53143,
+                                .time_hi_and_version = 20014,
+                                .clock_seq_hi_and_reserved = 139,
+                                .clock_seq_low = 27,
+                                .node = "v\025Oo9I"},
+                1);
+
+
+        uuid_test(
+                (struct tt_uuid){.time_low = 123421000,
+                                .time_mid = 36784,
+                                .time_hi_and_version = 11903,
+                                .clock_seq_hi_and_reserved = 175,
+                                .clock_seq_low = 80,
+                                .node = "Ad\325,b\353"},
+                (struct tt_uuid){.time_low = 532451999,
+                                .time_mid = 23976,
+                                .time_hi_and_version = 10437,
+                                .clock_seq_hi_and_reserved = 139,
+                                .clock_seq_low = 54,
+                                .node = "v\025Oo9I"},
+                -1);
+
+        return check_plan();
+}