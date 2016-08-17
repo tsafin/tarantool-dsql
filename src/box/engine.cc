gsmadhusudan/tarantool

Implement not covering indexes in vinyl
Fixes #1651
Fixes #1648

src/box/engine.cc
@@ -176,6 +176,11 @@ Handler::onAlter(Handler *)
 }
 
 void
+Handler::onAlterSpace(struct space *, struct space *)
+{
+}
+
+void
 Handler::executeSelect(struct txn *, struct space *space,
 		       uint32_t index_id, uint32_t iterator,
 		       uint32_t offset, uint32_t limit,

src/box/engine.h
@@ -205,6 +205,13 @@ public:
 		      struct port *);
 
 	virtual void onAlter(Handler *old);
+
+	/**
+	 * This method is called after altering space and
+	 * replacing old_space with new_space in space cache.
+	 */
+	virtual void onAlterSpace(struct space *old_space,
+				  struct space *new_space);
 	Engine *engine;
 };
 

tsafin/tarantool-dsql

src/box/engine.cc
@@ -174,7 +174,12 @@ Handler::executeUpsert(struct txn *, struct space *, struct request *)
 }
 
 void
-Handler::onAlter(Handler *)
+Handler::doAlterSpace(struct space *, struct space *)
+{
+}
+
+void
+Handler::commitAlterSpace(struct space *, struct space *)
 {
 }
 

src/box/engine.h
@@ -208,7 +208,15 @@ public:
 		      const char *key, const char *key_end,
 		      struct port *);
 
-	virtual void onAlter(Handler *old);
+	virtual void doAlterSpace(struct space *old_space,
+				  struct space *new_space);
+
+	/**
+	 * This method is called after altering space and
+	 * replacing old_space with new_space in space cache.
+	 */
+	virtual void commitAlterSpace(struct space *old_space,
+				      struct space *new_space);
 	Engine *engine;
 };
 