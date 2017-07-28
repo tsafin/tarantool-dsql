max-a-million/tarantool

sql: use region_alloc/join for temporary sql iproto tuples
obuf_alloc can not be used as a temporary buffer per sql row column,
because each column can be allocated in different slabs. It makes
impossible to allocate a port tuple from several intervals:
[col1_begin, col1_end], [col2_begin, col2_end], ...

Region has region_join method, which can realloc memory blocks from
different slabs in monolite memory area - [begin, end].
It allows to use tuple_alloc for port tuples.

Closes #2602

src/box/execute.h
@@ -98,12 +98,15 @@ sql_request_decode(struct sql_request *request, const char *data, uint32_t len,
  *
  * @param request IProto request.
  * @param out Out buffer of the iproto message.
+ * @param tmp_allocator Runtime allocator for temporary objects
+ *        (columns, tuples ...).
  *
  * @retval  0 Success.
  * @retval -1 Client or memory error.
  */
 int
-sql_prepare_and_execute(const struct sql_request *request, struct obuf *out);
+sql_prepare_and_execute(const struct sql_request *request, struct obuf *out,
+			struct region *tmp_allocator);
 
 #if defined(__cplusplus)
 } /* extern "C" { */

tsafin/tarantool-dsql

src/box/execute.h
@@ -98,12 +98,15 @@ sql_request_decode(struct sql_request *request, const char *data, uint32_t len,
  *
  * @param request IProto request.
  * @param out Out buffer of the iproto message.
+ * @param region Runtime allocator for temporary objects
+ *        (columns, tuples ...).
  *
  * @retval  0 Success.
  * @retval -1 Client or memory error.
  */
 int
-sql_prepare_and_execute(const struct sql_request *request, struct obuf *out);
+sql_prepare_and_execute(const struct sql_request *request, struct obuf *out,
+			struct region *region);
 
 #if defined(__cplusplus)
 } /* extern "C" { */