tarantool/tarantool

commit eb5cd536dee1d822c4726733436a83b21873ce3d

vinyl: do not track partial reads in tx manager
We use a binary tree for tracking reads (read_set). The tree is ordered
by (index, statement, tx). Insertion of a partial statement into the
tree may result in breaking comparison transitivity, which is mandated
by the tree implementation. However, for range requests we insert the
search key into the read set, which may be partial. This may result in a
crash in the read_set.

Tracking the search key doesn't make much sense anyway: it doesn't bring
us any closer to range request tracking, only obscures the semantics of
select. So let's track only statements actually returned by the iterator
plus gaps found by point requests.

Closes #2675

test/vinyl/mvcc.result
@@ -3758,46 +3758,13 @@ t:truncate()
 --
 -- Check that min/max/count transactions stay within a read view
 --
-c1:begin()
----
-- 
-...
-c1("t.index.pk:max()")
----
-- 
-...
-c2:begin()
----
-- 
-...
-c2("t:replace{1}")
----
-- - [1]
-...
-c2:commit()
----
-- 
-...
-c1("t.index.pk:max()") -- nothing
----
-- 
-...
-c1("t.index.pk:min()") -- nothing
----
-- 
-...
-c1("t.index.pk:count()") -- 0
----
-- - 0
-...
-c1:commit()
----
-- 
-...
---
 -- XXX bug: we read committed doata since there are no gap locks,
 -- and c1 is not turned into a read-only on conflict with c2.
 --
+t:replace{1}
+---
+- [1]
+...
 c1:begin()
 ---
 - 

test/vinyl/mvcc.test.lua
@@ -1502,19 +1502,10 @@ t:truncate()
 --
 -- Check that min/max/count transactions stay within a read view
 --
-c1:begin()
-c1("t.index.pk:max()")
-c2:begin()
-c2("t:replace{1}")
-c2:commit()
-c1("t.index.pk:max()") -- nothing
-c1("t.index.pk:min()") -- nothing
-c1("t.index.pk:count()") -- 0
-c1:commit()
---
 -- XXX bug: we read committed doata since there are no gap locks,
 -- and c1 is not turned into a read-only on conflict with c2.
 --
+t:replace{1}
 c1:begin()
 c1("t.index.pk:max()") -- {1}
 c1("t.index.pk:min()") -- {1}

src/box/vinyl.c
@@ -2707,21 +2707,15 @@ vy_index_get(struct vy_env *env, struct vy_tx *tx, struct vy_index *index,
 	struct vy_read_iterator itr;
 	vy_read_iterator_open(&itr, &env->run_env, index, tx,
 			      ITER_EQ, vykey, p_read_view);
-	if (vy_read_iterator_next(&itr, result) != 0)
-		goto error;
-	if (tx != NULL && vy_tx_track(tx, index, vykey, *result == NULL) != 0) {
-		vy_read_iterator_close(&itr);
-		goto error;
-	}
+	int rc = vy_read_iterator_next(&itr, result);
 	tuple_unref(vykey);
 	if (*result != NULL)
 		tuple_ref(*result);
 	vy_read_iterator_close(&itr);
+	if (rc != 0)
+		return -1;
 	rmean_collect(env->stat->rmean, VY_STAT_GET, 1);
 	return 0;
-error:
-	tuple_unref(vykey);
-	return -1;
 }
 
 /**
@@ -4925,9 +4919,6 @@ vy_cursor_next(struct vy_env *env, struct vy_cursor *c, struct tuple **result)
 	if (rc)
 		return -1;
 	c->n_reads++;
-	if (vy_tx_track(c->tx, index, vyresult ? vyresult : c->key,
-			vyresult == NULL))
-		return -1;
 	if (vyresult == NULL)
 		return 0;
 	if (c->need_check_eq &&

src/box/vy_point_iterator.c
@@ -457,8 +457,13 @@ restart:
 	}
 
 done:
-	if (rc == 0)
+	if (rc == 0) {
 		rc = vy_point_iterator_apply_history(itr, &history);
+		if (itr->tx != NULL)
+			rc = vy_tx_track(itr->tx, itr->index,
+					 (struct tuple *) itr->key,
+					 itr->curr_stmt == NULL);
+	}
 	*result = itr->curr_stmt;
 	vy_point_iterator_cleanup(&history, region_svp);
 	return rc;

src/box/vy_read_iterator.c
@@ -941,6 +941,9 @@ restart:
 		vy_cache_add(&itr->index->cache, *result, prev_key,
 			     itr->key, itr->iterator_type);
 
+	if (itr->tx != NULL && *result != NULL)
+		rc = vy_tx_track(itr->tx, itr->index, *result, false);
+
 clear:
 	if (prev_key != NULL)
 		tuple_unref(prev_key);

src/box/vy_tx.c
@@ -687,20 +687,25 @@ int
 vy_tx_track(struct vy_tx *tx, struct vy_index *index,
 	    struct tuple *key, bool is_gap)
 {
+	struct txv *v;
+	uint32_t part_count = tuple_field_count(key);
+
+	/* We do not support tracking range requests. */
+	assert(part_count >= index->cmp_def->part_count);
+
 	if (vy_tx_is_in_read_view(tx)) {
 		/* No point in tracking reads. */
 		return 0;
 	}
-	uint32_t part_count = tuple_field_count(key);
-	if (part_count >= index->cmp_def->part_count) {
-		struct txv *v = write_set_search_key(&tx->write_set, index, key);
-		if (v != NULL && (vy_stmt_type(v->stmt) == IPROTO_REPLACE ||
-				  vy_stmt_type(v->stmt) == IPROTO_DELETE)) {
-			/* Reading from own write set is serializable. */
-			return 0;
-		}
+
+	v = write_set_search_key(&tx->write_set, index, key);
+	if (v != NULL && (vy_stmt_type(v->stmt) == IPROTO_REPLACE ||
+			  vy_stmt_type(v->stmt) == IPROTO_DELETE)) {
+		/* Reading from own write set is serializable. */
+		return 0;
 	}
-	struct txv *v = read_set_search_key(&tx->xm->read_set, index, key, tx);
+
+	v = read_set_search_key(&tx->xm->read_set, index, key, tx);
 	if (v == NULL) {
 		v = txv_new(tx, index, key);
 		if (v == NULL)

tsafin/tarantool-dsql

commit 7178d01c33592f73358674c9d830024848468c8e

test: vinyl/mvcc: check uniqueness constraint for secondary indexes
The check was accidentally broken by commit eb5cd536dee1 ("vinyl: do not
track partial reads in tx manager"). Add a test case to avoid similar
screw-ups in future.

See #2716

test/vinyl/mvcc.result
@@ -3918,6 +3918,49 @@ c1:commit()
 ---
 - 
 ...
+t:truncate()
+---
+...
+--
+-- gh-2716 uniqueness check for secondary indexes
+--
+_ = t:create_index('sk', {parts = {2, 'unsigned'}, unique = true})
+---
+...
+c1:begin()
+---
+- 
+...
+c2:begin()
+---
+- 
+...
+c1("t:insert{1, 2}")
+---
+- - [1, 2]
+...
+c2("t:insert{2, 2}")
+---
+- - [2, 2]
+...
+c1:commit()
+---
+- 
+...
+c2:commit() -- rollback
+---
+- - {'error': 'Transaction has been aborted by conflict'}
+...
+t:select() -- {1, 2}
+---
+- - [1, 2]
+...
+t:truncate()
+---
+...
+t.index.sk:drop()
+---
+...
 -- *************************************************************************
 -- 1.7 cleanup marker: end of tests cleanup
 -- *************************************************************************

test/vinyl/mvcc.test.lua
@@ -1550,6 +1550,21 @@ c2("t:replace{2, 'new'}")
 c2:commit()
 c1("t:select()") -- {1, 'new'}, {2, 'new'}
 c1:commit()
+t:truncate()
+
+--
+-- gh-2716 uniqueness check for secondary indexes
+--
+_ = t:create_index('sk', {parts = {2, 'unsigned'}, unique = true})
+c1:begin()
+c2:begin()
+c1("t:insert{1, 2}")
+c2("t:insert{2, 2}")
+c1:commit()
+c2:commit() -- rollback
+t:select() -- {1, 2}
+t:truncate()
+t.index.sk:drop()
 
 -- *************************************************************************
 -- 1.7 cleanup marker: end of tests cleanup