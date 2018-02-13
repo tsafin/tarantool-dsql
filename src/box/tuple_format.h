glycerine/tarantool

Make action and nullability flags consistent
Replace is_nullable attribute in tuple_field and key_part w/ action
enum. This was needed since SQL supports different actions on constraint
violation.
New action field was added to tuple_field_def and key_part_def as well,
making is_nullable attribute and action working consistenly.
Extend nullability test.

Part of #2117

src/box/tuple_format.h
@@ -97,8 +97,8 @@ struct tuple_field {
 	bool is_key_part;
 	/** Tuple field name, specified by a user. Can be NULL. */
 	char *name;
-	/** True, if a field can store NULL. */
-	bool is_nullable;
+	/** Action to perform if NULL constraint failed. */
+	enum on_conflict_action action;
 };
 
 /**
@@ -109,7 +109,7 @@ struct tuple_field {
  */
 static inline bool
 tuple_field_is_nullable(const struct tuple_field *tuple_field) {
-	return tuple_field->is_nullable;
+	return tuple_field->action == ON_CONFLICT_ACTION_NONE;
 }
 
 struct mh_strnu32_t;

test/engine/null.test.lua
@@ -242,3 +242,100 @@ s.index.i4:select()
 s.index.i5:select()
 
 s:drop()
+
+-- Check that action property is consistent if nullable
+format = {}
+format[1] = { name = 'field1', type = 'unsigned' }
+format[2] = { name = 'field2', type = 'unsigned', is_nullable = false}
+s = box.schema.space.create('test', { engine = engine, format = format })
+pk = s:create_index('pk')
+
+format[2].action = 'abort'
+s:format(format) -- Ok.
+
+format[2].action = 'rollback'
+s:format(format) -- Ok.
+
+format[2].action = 'fail'
+s:format(format) -- Ok.
+
+format[2].action = 'ignore'
+s:format(format) -- Ok.
+
+format[2].action = 'replace'
+s:format(format) -- Ok.
+
+format[2].action = 'default'
+s:format(format) -- Ok.
+
+format[2].action = 'none'
+s:format(format) -- Fail.
+
+format[2].is_nullable = false
+format[2].action = 'abort'
+s:format(format) -- Fail.
+
+format[2].action = 'rollback'
+s:format(format) -- Fail.
+
+format[2].action = 'fail'
+s:format(format) -- Fail.
+
+format[2].action = 'ignore'
+s:format(format) -- Fail.
+
+format[2].action = 'replace'
+s:format(format) -- Fail.
+
+format[2].action = 'default'
+s:format(format) -- Fail.
+
+format[2].action = 'none'
+s:format(format) -- Ok.
+
+parts = {}
+parts[1] = {field = 2, type = 'unsigned', is_nullable = true, action = 'abort'}
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].action = 'rollback'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].action = 'fail'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].action = 'ignore'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].action = 'replace'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].action = 'default'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].action = 'none'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].is_nullable = false
+parts[1].action = 'none'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].action = 'abort'
+sk = s:create_index('sk', { parts = parts }) -- Ok.
+
+sk:drop()
+parts[1].action = 'rollback'
+sk = s:create_index('sk', { parts = parts }) -- Ok.
+
+sk:drop()
+parts[1].action = 'replace'
+sk = s:create_index('sk', { parts = parts }) -- Ok.
+
+sk:drop()
+parts[1].action = 'fail'
+sk = s:create_index('sk', { parts = parts }) -- Ok.
+
+sk:drop()
+parts[1].action = 'ignore'
+sk = s:create_index('sk', { parts = parts }) -- Ok.
+
+s:drop()

tsafin/tarantool-dsql

src/box/tuple_format.h
@@ -96,8 +96,8 @@ struct tuple_field {
 	int32_t offset_slot;
 	/** True if this field is used by an index. */
 	bool is_key_part;
-	/** True, if a field can store NULL. */
-	bool is_nullable;
+	/** Action to perform if NULL constraint failed. */
+	enum on_conflict_action nullable_action;
 };
 
 /**
@@ -109,7 +109,7 @@ struct tuple_field {
 static inline bool
 tuple_field_is_nullable(const struct tuple_field *tuple_field)
 {
-	return tuple_field->is_nullable;
+	return tuple_field->nullable_action == ON_CONFLICT_ACTION_NONE;
 }
 
 /**

test/engine/null.test.lua
@@ -242,3 +242,100 @@ s.index.i4:select()
 s.index.i5:select()
 
 s:drop()
+
+-- Check that action property is consistent if nullable
+format = {}
+format[1] = { name = 'field1', type = 'unsigned' }
+format[2] = { name = 'field2', type = 'unsigned', is_nullable = false}
+s = box.schema.space.create('test', { engine = engine, format = format })
+pk = s:create_index('pk')
+
+format[2].nullable_action = 'abort'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'rollback'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'fail'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'ignore'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'replace'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'default'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'none'
+s:format(format) -- Fail.
+
+format[2].is_nullable = false
+format[2].nullable_action = 'abort'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'rollback'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'fail'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'ignore'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'replace'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'default'
+s:format(format) -- Ok.
+
+format[2].nullable_action = 'none'
+s:format(format) -- Fail.
+
+parts = {}
+parts[1] = {field = 2, type = 'unsigned', is_nullable = true, nullable_action = 'abort'}
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].nullable_action = 'rollback'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].nullable_action = 'fail'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].nullable_action = 'ignore'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].nullable_action = 'replace'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].nullable_action = 'default'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].nullable_action = 'none'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].is_nullable = false
+parts[1].nullable_action = 'none'
+sk = s:create_index('sk', { parts = parts }) -- Fail.
+
+parts[1].nullable_action = 'abort'
+sk = s:create_index('sk', { parts = parts }) -- Ok.
+
+sk:drop()
+parts[1].nullable_action = 'rollback'
+sk = s:create_index('sk', { parts = parts }) -- Ok.
+
+sk:drop()
+parts[1].nullable_action = 'replace'
+sk = s:create_index('sk', { parts = parts }) -- Ok.
+
+sk:drop()
+parts[1].nullable_action = 'fail'
+sk = s:create_index('sk', { parts = parts }) -- Ok.
+
+sk:drop()
+parts[1].nullable_action = 'ignore'
+sk = s:create_index('sk', { parts = parts }) -- Ok.
+
+s:drop()