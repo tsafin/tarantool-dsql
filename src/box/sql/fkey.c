AKEB/tarantool

sql: delegate SQL primary key check to Tarantool
As last point of removing colflag attribute from struct Column,
table_column_is_primkey() function is added. It checks via Tarantool
facilities whether column belongs to PK or not (after space creation).
However, during initializing of SQL iternals and before actual creation
of struct space, colflag attribute (as indicator of PK) still can be used.
Since this is the only purpose of attribute, one has been renamed.

Closes #3118

src/box/sql/fkey.c
@@ -865,7 +865,7 @@ fkParentIsModified(Table * pTab, FKey * p, int *aChange)
 					if (0 ==
 					    strcmp(pCol->zName, zKey))
 						return 1;
-				} else if (pCol->colFlags & COLFLAG_PRIMKEY) {
+				} else if (table_column_is_primkey(pTab, iKey)) {
 					return 1;
 				}
 			}

src/box/sql/pragma.c
@@ -352,8 +352,7 @@ sqlite3Pragma(Parse * pParse, Token * pId,	/* First part of [schema.]id field */
 				sqlite3ViewGetColumnNames(pParse, pTab);
 				for (i = 0, pCol = pTab->aCol; i < pTab->nCol;
 				     i++, pCol++) {
-					if ((pCol->
-					     colFlags & COLFLAG_PRIMKEY) == 0) {
+					if (!table_column_is_primkey(pTab, i)) {
 						k = 0;
 					} else if (pPk == 0) {
 						k = 1;

src/box/sql/sqliteInt.h
@@ -1354,12 +1354,8 @@ struct Column {
 	u8 notNull;		/* An OE_ code for handling a NOT NULL constraint */
 	char affinity;		/* One of the SQLITE_AFF_... values */
 	u8 szEst;		/* Estimated size of value in this column. sizeof(INT)==1 */
-	u8 colFlags;		/* Boolean properties.  See COLFLAG_ defines below */
+	u8 is_primkey;		/* Boolean propertie for being PK */
 };
-
-/* Allowed values for Column.colFlags:
- */
-#define COLFLAG_PRIMKEY  0x0001	/* Column is part of the primary key */
 
 /*
  * A sort order can be either ASC or DESC.
@@ -3100,6 +3096,7 @@ void sqlite3ResetAllSchemasOfConnection(sqlite3 *);
 void sqlite3ResetOneSchema(sqlite3 *);
 void sqlite3CommitInternalChanges();
 void sqlite3DeleteColumnNames(sqlite3 *, Table *);
+bool table_column_is_primkey(Table *, uint32_t);
 int sqlite3ColumnsFromExprList(Parse *, ExprList *, i16 *, Column **);
 void sqlite3SelectAddColumnTypeAndCollation(Parse *, Table *, Select *);
 Table *sqlite3ResultSetOfSelect(Parse *, Select *);

src/box/sql/update.c
@@ -232,12 +232,9 @@ sqlite3Update(Parse * pParse,		/* The parser context */
 			goto update_cleanup;
 		}
 		for (j = 0; j < pTab->nCol; j++) {
-			if (strcmp
-			    (pTab->aCol[j].zName, pChanges->a[i].zName) == 0) {
-				if (pPk
-					   && (pTab->aCol[j].
-					       colFlags & COLFLAG_PRIMKEY) !=
-					   0) {
+			if (strcmp(pTab->aCol[j].zName,
+				   pChanges->a[i].zName) == 0) {
+				if (pPk && table_column_is_primkey(pTab, j)) {
 					chngPk = 1;
 				}
 				aXRef[j] = i;
@@ -490,7 +487,7 @@ sqlite3Update(Parse * pParse,		/* The parser context */
 		for (i = 0; i < pTab->nCol; i++) {
 			if (oldmask == 0xffffffff
 			    || (i < 32 && (oldmask & MASKBIT32(i)) != 0)
-			    || (pTab->aCol[i].colFlags & COLFLAG_PRIMKEY) != 0) {
+			    || table_column_is_primkey(pTab, i)) {
 				testcase(oldmask != 0xffffffff && i == 31);
 				sqlite3ExprCodeGetColumnOfTable(v, pTab,
 								iDataCur, i,

tsafin/tarantool-dsql

src/box/sql/fkey.c
@@ -866,7 +866,7 @@ fkParentIsModified(Table * pTab, FKey * p, int *aChange)
 					if (0 ==
 					    strcmp(pCol->zName, zKey))
 						return 1;
-				} else if (pCol->colFlags & COLFLAG_PRIMKEY) {
+				} else if (table_column_is_in_pk(pTab, iKey)) {
 					return 1;
 				}
 			}

src/box/sql/pragma.c
@@ -383,8 +383,7 @@ sqlite3Pragma(Parse * pParse, Token * pId,	/* First part of [schema.]id field */
 				sqlite3ViewGetColumnNames(pParse, pTab);
 				for (i = 0, pCol = pTab->aCol; i < pTab->nCol;
 				     i++, pCol++) {
-					if ((pCol->
-					     colFlags & COLFLAG_PRIMKEY) == 0) {
+					if (!table_column_is_in_pk(pTab, i)) {
 						k = 0;
 					} else if (pPk == 0) {
 						k = 1;

src/box/sql/sqliteInt.h
@@ -1357,12 +1357,8 @@ struct Column {
 					   */
 	char affinity;		/* One of the SQLITE_AFF_... values */
 	u8 szEst;		/* Estimated size of value in this column. sizeof(INT)==1 */
-	u8 colFlags;		/* Boolean properties.  See COLFLAG_ defines below */
+	u8 is_primkey;		/* Boolean propertie for being PK */
 };
-
-/* Allowed values for Column.colFlags:
- */
-#define COLFLAG_PRIMKEY  0x0001	/* Column is part of the primary key */
 
 /*
  * A sort order can be either ASC or DESC.
@@ -3079,6 +3075,7 @@ void sqlite3ResetAllSchemasOfConnection(sqlite3 *);
 void sqlite3ResetOneSchema(sqlite3 *);
 void sqlite3CommitInternalChanges();
 void sqlite3DeleteColumnNames(sqlite3 *, Table *);
+bool table_column_is_in_pk(Table *, uint32_t);
 int sqlite3ColumnsFromExprList(Parse *, ExprList *, i16 *, Column **);
 void sqlite3SelectAddColumnTypeAndCollation(Parse *, Table *, Select *);
 Table *sqlite3ResultSetOfSelect(Parse *, Select *);

src/box/sql/update.c
@@ -232,12 +232,9 @@ sqlite3Update(Parse * pParse,		/* The parser context */
 			goto update_cleanup;
 		}
 		for (j = 0; j < pTab->nCol; j++) {
-			if (strcmp
-			    (pTab->aCol[j].zName, pChanges->a[i].zName) == 0) {
-				if (pPk
-					   && (pTab->aCol[j].
-					       colFlags & COLFLAG_PRIMKEY) !=
-					   0) {
+			if (strcmp(pTab->aCol[j].zName,
+				   pChanges->a[i].zName) == 0) {
+				if (pPk && table_column_is_in_pk(pTab, j)) {
 					chngPk = 1;
 				}
 				aXRef[j] = i;
@@ -492,7 +489,7 @@ sqlite3Update(Parse * pParse,		/* The parser context */
 		for (i = 0; i < pTab->nCol; i++) {
 			if (oldmask == 0xffffffff
 			    || (i < 32 && (oldmask & MASKBIT32(i)) != 0)
-			    || (pTab->aCol[i].colFlags & COLFLAG_PRIMKEY) != 0) {
+			    || table_column_is_in_pk(pTab, i)) {
 				testcase(oldmask != 0xffffffff && i == 31);
 				sqlite3ExprCodeGetColumnOfTable(v, pTab,
 								iDataCur, i,