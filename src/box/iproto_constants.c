vanyail/tarantool

iproto: rename IPROTO_SQL_OPTIONS to IPROTO_OPTIONS
IPROTO_OPTIONS can be used in the future for request types,
different from IPROTO_EXECUTE.

src/box/iproto_constants.c
@@ -64,7 +64,7 @@ const unsigned char iproto_key_type[IPROTO_KEY_MAX] =
 	/* }}} */
 
 	/* {{{ unused */
-		/* 0x16 */	MP_UINT,
+		/* 0x16 */	MP_MAP,
 		/* 0x17 */	MP_UINT,
 		/* 0x18 */	MP_UINT,
 		/* 0x19 */	MP_UINT,
@@ -144,7 +144,7 @@ const char *iproto_key_strs[IPROTO_KEY_MAX] = {
 	"offset",           /* 0x13 */
 	"iterator",         /* 0x14 */
 	"index base",       /* 0x15 */
-	NULL,               /* 0x16 */
+	"options",          /* 0x16 */
 	NULL,               /* 0x17 */
 	NULL,               /* 0x18 */
 	NULL,               /* 0x19 */
@@ -188,8 +188,7 @@ const char *iproto_key_strs[IPROTO_KEY_MAX] = {
 	NULL,               /* 0x3f */
 	"SQL text",         /* 0x40 */
 	"SQL bind",         /* 0x41 */
-	"SQL options",      /* 0x42 */
-	"SQL info",         /* 0x43 */
+	"SQL info",         /* 0x42 */
 };
 
 const char *vy_page_info_key_strs[VY_PAGE_INFO_KEY_MAX] = {

src/box/iproto_constants.h
@@ -66,6 +66,7 @@ enum iproto_key {
 	IPROTO_OFFSET = 0x13,
 	IPROTO_ITERATOR = 0x14,
 	IPROTO_INDEX_BASE = 0x15,
+	IPROTO_OPTIONS = 0x16,
 
 	/* Leave a gap between integer values and other keys */
 	IPROTO_KEY = 0x20,
@@ -104,13 +105,12 @@ enum iproto_key {
 	/* Leave a gap between response keys and SQL keys. */
 	IPROTO_SQL_TEXT = 0x40,
 	IPROTO_SQL_BIND = 0x41,
-	IPROTO_SQL_OPTIONS = 0x42,
 	/**
 	 * IPROTO_SQL_INFO: {
 	 *     SQL_ROW_COUNT: number
 	 * }
 	 */
-	IPROTO_SQL_INFO = 0x43,
+	IPROTO_SQL_INFO = 0x42,
 	IPROTO_KEY_MAX
 };
 

tsafin/tarantool-dsql

src/box/iproto_constants.c
@@ -86,6 +86,7 @@ const unsigned char iproto_key_type[IPROTO_KEY_MAX] =
 	/* 0x26 */	MP_MAP, /* IPROTO_VCLOCK */
 	/* 0x27 */	MP_STR, /* IPROTO_EXPR */
 	/* 0x28 */	MP_ARRAY, /* IPROTO_OPS */
+	/* 0x29 */	MP_MAP, /* IPROTO_OPTIONS */
 	/* }}} */
 };
 
@@ -166,7 +167,7 @@ const char *iproto_key_strs[IPROTO_KEY_MAX] = {
 	"vector clock",     /* 0x26 */
 	"expression",       /* 0x27 */
 	"operations",       /* 0x28 */
-	NULL,               /* 0x29 */
+	"options",          /* 0x29 */
 	NULL,               /* 0x2a */
 	NULL,               /* 0x2b */
 	NULL,               /* 0x2c */
@@ -191,8 +192,7 @@ const char *iproto_key_strs[IPROTO_KEY_MAX] = {
 	NULL,               /* 0x3f */
 	"SQL text",         /* 0x40 */
 	"SQL bind",         /* 0x41 */
-	"SQL options",      /* 0x42 */
-	"SQL info",         /* 0x43 */
+	"SQL info",         /* 0x42 */
 };
 
 const char *vy_page_info_key_strs[VY_PAGE_INFO_KEY_MAX] = {

src/box/iproto_constants.h
@@ -87,6 +87,7 @@ enum iproto_key {
 	/* Also request keys. See the comment above. */
 	IPROTO_EXPR = 0x27, /* EVAL */
 	IPROTO_OPS = 0x28, /* UPSERT but not UPDATE ops, because of legacy */
+	IPROTO_OPTIONS = 0x29,
 
 	/* Leave a gap between request keys and response keys */
 	IPROTO_DATA = 0x30,
@@ -103,13 +104,12 @@ enum iproto_key {
 	/* Leave a gap between response keys and SQL keys. */
 	IPROTO_SQL_TEXT = 0x40,
 	IPROTO_SQL_BIND = 0x41,
-	IPROTO_SQL_OPTIONS = 0x42,
 	/**
 	 * IPROTO_SQL_INFO: {
 	 *     SQL_INFO_ROW_COUNT: number
 	 * }
 	 */
-	IPROTO_SQL_INFO = 0x43,
+	IPROTO_SQL_INFO = 0x42,
 	IPROTO_KEY_MAX
 };
 