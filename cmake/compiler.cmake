tarantool/tarantool

commit 8c538963a78f22746233b22648fb2aa6933dc135

Fix Debug build on GCC 8
Fixed false positive -Wimplicit-fallthrough in http_parser.c by adding a
break. The code jumps anyway, so the execution flow is not changed.

Fixed false positive -Wparenthesis in reflection.h by removing the
parentheses. The argument 'method' of the macro 'type_foreach_method' is
just name of the loop variable and is passed to the macro for
readability reasons.

Fixed false positive -Wcast-function-type triggered by reflection.h by
adding -Wno-cast-function-type for sources and unit tests. We cast a
pointer to a member function to an another pointer to member function to
store it in a structure, but we cast it back before made a call. It is
legal and does not lead to an undefined behaviour.

Fixes #3685.

src/CMakeLists.txt
@@ -3,6 +3,14 @@
 #
 enable_tnt_compile_flags()
 
+# Suppress noise GCC 8 warnings.
+#
+# reflection.h casts a pointer to a member function to an another pointer to a
+# member function to store it in a structure, but cast it back before a call.
+# It is legal and does not lead to an undefined behaviour.
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-cast-function-type")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-cast-function-type")
+
 include_directories(${LIBEV_INCLUDE_DIR})
 include_directories(${LIBEIO_INCLUDE_DIR})
 include_directories(${LIBCORO_INCLUDE_DIR})

test/unit/CMakeLists.txt
@@ -5,6 +5,14 @@ endif()
 file(GLOB all_sources *.c *.cc)
 set_source_files_compile_flags(${all_sources})
 
+# Suppress noise GCC 8 warnings.
+#
+# reflection.h casts a pointer to a member function to an another pointer to a
+# member function to store it in a structure, but cast it back before a call.
+# It is legal and does not lead to an undefined behaviour.
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-cast-function-type")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-cast-function-type")
+
 include_directories(${PROJECT_SOURCE_DIR}/src)
 include_directories(${PROJECT_BINARY_DIR}/src)
 include_directories(${PROJECT_SOURCE_DIR}/src/box)

src/http_parser.c
@@ -292,6 +292,7 @@ http_parse_header_line(struct http_parser *prsr, char **bufp,
 			default:
 				return HTTP_PARSE_INVALID;
 			}
+			break;
 		/* http_header name */
 		case sw_name:
 			c = lowcase[ch];

src/reflection.h
@@ -106,10 +106,10 @@ struct method_info {
 	};
 };
 
-#define type_foreach_method(m, method)					\
+#define type_foreach_method(m, method)						\
 	for(const struct type_info *_m = (m); _m != NULL; _m = _m->parent)	\
-		for (const struct method_info *(method) = _m->methods;	\
-		     (method)->name != NULL; (method)++)
+		for (const struct method_info *method = _m->methods;		\
+		     method->name != NULL; method++)
 
 inline const struct method_info *
 type_method_by_name(const struct type_info *type, const char *name)

tsafin/tarantool-dsql

commit 54360ffccebbd54cd547634f5d9bbfb267cffac5

Fix clang build (fails on -Wno-cast-function-type)
The problem is that clang does not support -Wno-cast-function-type flag.
It is the regression from 8c538963a78f22746233b22648fb2aa6933dc135.

Follow up of #3685.
Fixes #3701.

cmake/compiler.cmake
@@ -95,6 +95,7 @@ check_c_compiler_flag("-Wno-varargs" CC_HAS_WNO_VARARGS)
 check_c_compiler_flag("-Wno-char-subscripts" CC_HAS_WNO_CHAR_SUBSCRIPTS)
 check_c_compiler_flag("-Wno-format-truncation" CC_HAS_WNO_FORMAT_TRUNCATION)
 check_c_compiler_flag("-Wno-implicit-fallthrough" CC_HAS_WNO_IMPLICIT_FALLTHROUGH)
+check_c_compiler_flag("-Wno-cast-function-type" CC_HAS_WNO_CAST_FUNCTION_TYPE)
 
 #
 # Perform build type specific configuration.
@@ -245,6 +246,15 @@ macro(enable_tnt_compile_flags)
     if (CMAKE_COMPILER_IS_GNUCC)
         # A workaround for Redhat Developer Toolset 2.x on RHEL/CentOS 5.x
         add_compile_flags("C" "-fno-gnu89-inline")
+    endif()
+
+    # Suppress noise GCC 8 warnings.
+    #
+    # reflection.h casts a pointer to a member function to an another pointer
+    # to a member function to store it in a structure, but cast it back before
+    # a call. It is legal and does not lead to an undefined behaviour.
+    if (CC_HAS_WNO_CAST_FUNCTION_TYPE)
+        add_compile_flags("C;CXX" "-Wno-cast-function-type")
     endif()
 
     add_definitions("-D__STDC_FORMAT_MACROS=1")

src/CMakeLists.txt
@@ -3,14 +3,6 @@
 #
 enable_tnt_compile_flags()
 
-# Suppress noise GCC 8 warnings.
-#
-# reflection.h casts a pointer to a member function to an another pointer to a
-# member function to store it in a structure, but cast it back before a call.
-# It is legal and does not lead to an undefined behaviour.
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-cast-function-type")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-cast-function-type")
-
 include_directories(${LIBEV_INCLUDE_DIR})
 include_directories(${LIBEIO_INCLUDE_DIR})
 include_directories(${LIBCORO_INCLUDE_DIR})

test/unit/CMakeLists.txt
@@ -5,14 +5,6 @@ endif()
 file(GLOB all_sources *.c *.cc)
 set_source_files_compile_flags(${all_sources})
 
-# Suppress noise GCC 8 warnings.
-#
-# reflection.h casts a pointer to a member function to an another pointer to a
-# member function to store it in a structure, but cast it back before a call.
-# It is legal and does not lead to an undefined behaviour.
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-cast-function-type")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-cast-function-type")
-
 include_directories(${PROJECT_SOURCE_DIR}/src)
 include_directories(${PROJECT_BINARY_DIR}/src)
 include_directories(${PROJECT_SOURCE_DIR}/src/box)