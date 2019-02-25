vakalykv/tarantool

sql: re-enable ORDER BY field filter optimization
When we replaced SQLite's ephemeral spaces with our ones, one
optimization concerning ORDER BY clause was disabled. It allows to
reduce number of fields in format of ephemeral space or sorter table.
To illustrate how it works, consider example:

CREATE TABLE t (id INT PRIMARY KEY, b INT);
SELECT * FROM t ORDER BY b;

To sort entries from t, ephemeral space with format [b, id, b] is
created. One can see, that such format contains duplicate of b column.
To avoid such situation, SQLite provides optimization which removes
duplicates. Meanwhile, it doesn't change already set format of ephemeral
(or sorter) space (and SQLite tolerates that format difference). That's
why it was turned off. However, such optimization turns out to be not
optional but required: some column values shouldn't be computed twice.
For instance:

SELECT random() AS x FROM t ORDER BY x;

Without filtering fields from ephemeral space format, it would be like:
[random(), random()]. In other words, results would be sorted by first
call to random() function, but resulting set would consist of values
given by second call of random(). So, to enable it, we should reduce
field count in format of ephemeral space by number of matches between
SELECT and ORDER BY column lists.

Also, type of return value for random() function has been fixed.

Closes #3783

src/box/sql/select.c
@@ -975,23 +975,33 @@ selectInnerLoop(Parse * pParse,		/* The parser context */
 			 * saving space and CPU cycles.
 			 */
 			ecelFlags |= (SQLITE_ECEL_OMITREF | SQLITE_ECEL_REF);
-			/* This optimization is temporary disabled. It seems
-			 * that it was possible to create table with n columns and
-			 * insert tuple with m columns, where m < n. Contrary, Tarantool
-			 * doesn't allow to alter the number of fields in tuple
-			 * to be inserted. This may be uncommented by delaying the
-			 * creation of table until insertion of first tuple,
-			 * so that the number of fields in tuple can be precisely calculated.
+			/*
+			 * Format for ephemeral space has been
+			 * already set with field count calculated
+			 * as orderBy->nExpr + pEList->nExpr + 1,
+			 * where pEList is a select list.
+			 * Since we want to reduce number of fields
+			 * in format of ephemeral space, we should
+			 * fix corresponding opcode's argument.
+			 * Otherwise, tuple format won't match
+			 * space format.
 			 */
-			/*for (i = pSort->nOBSat; i < pSort->pOrderBy->nExpr; i++) {
-				int j;
-				if ((j =
-				     pSort->pOrderBy->a[i].u.x.iOrderByCol) >
-				    0) {
+			uint32_t excess_field_count = 0;
+			for (i = pSort->nOBSat; i < pSort->pOrderBy->nExpr;
+			     i++) {
+				int j = pSort->pOrderBy->a[i].u.x.iOrderByCol;
+				if (j > 0) {
+					excess_field_count++;
 					pEList->a[j - 1].u.x.iOrderByCol =
-					    (u16) (i + 1 - pSort->nOBSat);
+						(u16) (i + 1 - pSort->nOBSat);
 				}
-			}*/
+			}
+			struct VdbeOp *open_eph_op =
+				sqlite3VdbeGetOp(v, pSort->addrSortIndex);
+			assert(open_eph_op->p2 - excess_field_count > 0);
+			sqlite3VdbeChangeP2(v, pSort->addrSortIndex,
+					    open_eph_op->p2 -
+					    excess_field_count);
 			regOrig = 0;
 			assert(eDest == SRT_Set || eDest == SRT_Mem
 			       || eDest == SRT_Coroutine

tsafin/tarantool-dsql

src/box/sql/select.c
@@ -974,23 +974,33 @@ selectInnerLoop(Parse * pParse,		/* The parser context */
 			 * saving space and CPU cycles.
 			 */
 			ecelFlags |= (SQL_ECEL_OMITREF | SQL_ECEL_REF);
-			/* This optimization is temporary disabled. It seems
-			 * that it was possible to create table with n columns and
-			 * insert tuple with m columns, where m < n. Contrary, Tarantool
-			 * doesn't allow to alter the number of fields in tuple
-			 * to be inserted. This may be uncommented by delaying the
-			 * creation of table until insertion of first tuple,
-			 * so that the number of fields in tuple can be precisely calculated.
+			/*
+			 * Format for ephemeral space has been
+			 * already set with field count calculated
+			 * as orderBy->nExpr + pEList->nExpr + 1,
+			 * where pEList is a select list.
+			 * Since we want to reduce number of fields
+			 * in format of ephemeral space, we should
+			 * fix corresponding opcode's argument.
+			 * Otherwise, tuple format won't match
+			 * space format.
 			 */
-			/*for (i = pSort->nOBSat; i < pSort->pOrderBy->nExpr; i++) {
-				int j;
-				if ((j =
-				     pSort->pOrderBy->a[i].u.x.iOrderByCol) >
-				    0) {
+			uint32_t excess_field_count = 0;
+			for (i = pSort->nOBSat; i < pSort->pOrderBy->nExpr;
+			     i++) {
+				int j = pSort->pOrderBy->a[i].u.x.iOrderByCol;
+				if (j > 0) {
+					excess_field_count++;
 					pEList->a[j - 1].u.x.iOrderByCol =
-					    (u16) (i + 1 - pSort->nOBSat);
+						(u16) (i + 1 - pSort->nOBSat);
 				}
-			}*/
+			}
+			struct VdbeOp *open_eph_op =
+				sqlVdbeGetOp(v, pSort->addrSortIndex);
+			assert(open_eph_op->p2 - excess_field_count > 0);
+			sqlVdbeChangeP2(v, pSort->addrSortIndex,
+					open_eph_op->p2 -
+					excess_field_count);
 			regOrig = 0;
 			assert(eDest == SRT_Set || eDest == SRT_Mem
 			       || eDest == SRT_Coroutine