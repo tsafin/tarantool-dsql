tarantool/tarantool

commit 17df9edf013fd262be1b2ffd34d5080d20bc716e

tarantoolctl: allow to start instances with delayed box.cfg{}
`tarantoolctl start` patches box.cfg two times:
1) before the init script to set default values and enforce some others,
2) after the init script to prevent changing a pid_file in runtime.

The second patching fails if an init file does not call
box.cfg{} before it's finished. This can take a place in apps with
managed instances which receive configuration from external server.

This patch moves the second patching into the box.cfg
wrapper created during the first patching. So the second patching
is performed only after box.cfg{} was invoked, so it does not fail anymore.

However there is relatively minor flaw for applications that
invoke box.cfg{} after init script is finished:
`tarantoolctl start` goes to background only when box.cfg{} is called.
Though this is not the case for daemon management systems like systemd,
as they handle backgrounding on their side

Fixes #4435

@TarantoolBot document
Title: tarantoolctl allows to start instances without a box.cfg{}

tarantoolctl now works for instances without box.cfg{} or
with delayed box.cfg{} call. This can be managed instances which receive
configuration from external server.

For such instances `tarantoolctl start` goes to background when
box.cfg{} is called, so it will wait until options for box.cfg are received.
However this is not the case for daemon management systems like systemd,
as they handle backgrounding on their side.

extra/dist/tarantoolctl.in
@@ -483,6 +483,16 @@ local function wrapper_cfg(cfg)
         os.exit(1)
     end
 
+    -- Prevent overwriting pid_file in subsequent box.cfg calls.
+    local box_cfg_mt = getmetatable(box.cfg)
+    local orig_cfg_call = box_cfg_mt.__call
+    box_cfg_mt.__call = function(old_cfg, new_cfg)
+        if old_cfg.pid_file ~= nil and new_cfg ~= nil and new_cfg.pid_file ~= nil then
+            new_cfg.pid_file = old_cfg.pid_file
+        end
+        orig_cfg_call(old_cfg, new_cfg)
+    end
+
     return data
 end
 
@@ -546,18 +556,6 @@ local function start()
             end
         end
         os.exit(1)
-    end
-    local box_cfg_mt = getmetatable(box.cfg)
-    if box_cfg_mt == nil then
-        log.error('box.cfg() is not called in an instance file')
-        os.exit(1)
-    end
-    local old_call = box_cfg_mt.__call
-    box_cfg_mt.__call = function(old_cfg, cfg)
-        if old_cfg.pid_file ~= nil and cfg ~= nil and cfg.pid_file ~= nil then
-            cfg.pid_file = old_cfg.pid_file
-        end
-        old_call(old_cfg, cfg)
     end
     return 0
 end

tsafin/tarantool-dsql

commit 3a63f1644be8e705a7676ffefbf41f4ddc8b9cbb

test: tarantoolctl: verify delayed box.cfg()
app-tap.tarantoolctl.test.lua fails after
17df9edf013fd262be1b2ffd34d5080d20bc716e ('tarantoolctl: allow to start
instances with delayed box.cfg{}').

The commit fixes the test case that did check that an error is reported
if box.cfg() was not called in an instance script.

Follows up #4435.
Fixes #4448.

test/app-tap/tarantoolctl.test.lua
@@ -179,13 +179,18 @@ test:plan(8)
 do
     local dir = fio.tempdir()
     create_script(dir, 'script.lua', [[ box.cfg{memtx_memory = 104857600} ]])
-    create_script(dir, 'no_box_cfg.lua', [[ print('Hi!') ]])
+    create_script(dir, 'delayed_box_cfg.lua', [[
+        local fiber = require('fiber')
+        fiber.create(function()
+            fiber.sleep(1)
+            box.cfg{}
+        end)
+    ]])
 
     local status, err = pcall(function()
         test:test("basic test", function(test_i)
             test_i:plan(18)
-            check_ok(test_i, dir, 'start', 'no_box_cfg', 1, nil, "Starting instance",
-                     "box.cfg() is not called in an instance file")
+            check_ok(test_i, dir, 'start', 'delayed_box_cfg', 0, nil, "Starting instance")
             check_ok(test_i, dir, 'start',  'script', 0, nil, "Starting instance")
             tctl_wait_start(dir, 'script')
             check_ok(test_i, dir, 'status', 'script', 0, nil, "is running")