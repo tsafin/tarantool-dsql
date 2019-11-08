tarantool/tarantool

commit 96dbc49d097a96af5273cce2b5663db5917f4ea9

box/console: Refactor command handling
We will need to parse and verify "\set" commands
in remote console text-wire protocol thus to not
duplicate the code lets factor helper out for
reuse sake.

Part-of #3834

Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

src/box/lua/console.lua
@@ -162,11 +162,16 @@ local function output_save(fmt, opts)
     }
 end
 
-local function format(status, ...)
+local function current_output()
     local d = box.session.storage.console_output_format
     if d == nil then
-        d = default_output_format
+        return default_output_format
     end
+    return d
+end
+
+local function format(status, ...)
+    local d = current_output()
     return output_handlers[d["fmt"]](status, d["opts"], ...)
 end
 
@@ -257,19 +262,44 @@ local operators = {
     q = quit
 }
 
-local function preprocess(storage, line)
+--
+-- Generate command arguments from the line to
+-- be passed into command handlers.
+local function parse_operators(line)
     local items = {}
     for item in string.gmatch(line, '([^%s]+)') do
         items[#items + 1] = item
     end
     if #items == 0 then
-        return help_wrapper()
+        return 0, nil
     end
     if operators[items[1]] == nil then
+        return #items, nil
+    end
+    return #items, items
+end
+
+--
+-- Preprocess a command via operator helpers.
+local function preprocess(storage, line)
+    local nr_items, items = parse_operators(line)
+    if nr_items == 0 then
+        return help_wrapper()
+    end
+    if items == nil then
         local msg = "Invalid command \\%s. Type \\help for help."
         return format(false, msg:format(items[1]))
     end
     return operators[items[1]](storage, unpack(items))
+end
+
+--
+-- Return a command without a leading slash.
+local function get_command(line)
+    if line:sub(1, 1) == '\\' then
+        return line:sub(2)
+    end
+    return nil
 end
 
 --
@@ -279,8 +309,9 @@ local function local_eval(storage, line)
     if not line then
         return nil
     end
-    if line:sub(1, 1) == '\\' then
-        return preprocess(storage, line:sub(2))
+    local command = get_command(line)
+    if command then
+        return preprocess(storage, command)
     end
     if storage.language == 'sql' then
         return format(pcall(box.execute, line))

tsafin/tarantool-dsql

commit 3c2d7d2e4d5963a1949bb09eed39961799d39d7c

box/console: fix abnormal exit after unknown command
When invalid command is passed we should send an error message to a
client. Instead a nil dereference occurs that causes abnormal exit of a
console.

This is the regression from 96dbc49d097a96af5273cce2b5663db5917f4ea9
('box/console: Refactor command handling').

Reported-by: Mergen Imeev <imeevma@tarantool.org>
Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
Reviewed-by: Alexander Turenko <alexander.turenko@tarantool.org>

src/box/lua/console.lua
@@ -323,7 +323,7 @@ local function preprocess(storage, line)
     end
     if items == nil then
         local msg = "Invalid command \\%s. Type \\help for help."
-        return format(false, msg:format(items[1]))
+        return format(false, msg:format(line))
     end
     return operators[items[1]](storage, unpack(items))
 end

test/app-tap/console.test.lua
@@ -21,7 +21,7 @@ local EOL = "\n...\n"
 
 test = tap.test("console")
 
-test:plan(72)
+test:plan(73)
 
 -- Start console and connect to it
 local server = console.listen(CONSOLE_SOCKET)
@@ -290,6 +290,20 @@ client = socket.tcp_connect("unix/", CONSOLE_SOCKET)
 _ = client:read(128)
 client:write("box.session.type();\n")
 test:is(yaml.decode(client:read(EOL))[1], "console", "session type")
+client:close()
+
+--
+-- An unknown backslash started command causes abnormal exit of
+-- a console.
+--
+local cmd = '\\unknown_command'
+local exp_res = {error = string.format(
+    'Invalid command %s. Type \\help for help.', cmd)}
+client = socket.tcp_connect("unix/", CONSOLE_SOCKET)
+client:read(128)
+client:write(('%s\n'):format(cmd))
+local res = yaml.decode(client:read(EOL))[1]
+test:is_deeply(res, exp_res, 'unknown command')
 client:close()
 
 server:close()