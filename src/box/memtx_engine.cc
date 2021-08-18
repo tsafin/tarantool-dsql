oleggator/tarantool

memtx: implement api for memory allocator selection
Add new 'memtx_allocator' option to box.cfg{} which allows to
select the appropriate allocator for memtx tuples if necessary.
Possible values are "system" for malloc allocator and
"small" for default small allocator.
Closes #5419

@TarantoolBot document
Title: Add new 'memtx_allocator' option to box.cfg{}
Add new 'memtx_allocator' option to box.cfg{} which allows to
select the appropriate allocator for memtx tuples if necessary.
Possible values are "system" for malloc allocator and
"small" for default small allocator.

src/box/memtx_engine.cc
@@ -91,6 +91,8 @@ memtx_engine_get_alloc(struct memtx_engine *memtx)
 {
 	if (std::is_same<ALLOC, SmallAlloc>::value)
 		return &memtx->alloc;
+	else if (std::is_same<ALLOC, SysAlloc>::value)
+		return &memtx->sys_alloc;
 	return NULL;
 }
 
@@ -100,6 +102,8 @@ memtx_engine_get_delayed_lifo(struct memtx_engine *memtx)
 {
 	if (std::is_same<ALLOC, SmallAlloc>::value)
 		return &memtx->delayed;
+	else if (std::is_same<ALLOC, SysAlloc>::value)
+		return &memtx->sys_delayed;
 	return NULL;
 }
 
@@ -228,6 +232,8 @@ memtx_engine_shutdown(struct engine *engine)
 	small_alloc_destroy(&memtx->alloc);
 	slab_cache_destroy(&memtx->slab_cache);
 	tuple_arena_destroy(&memtx->arena);
+	memtx_free_all_tuples_from_lifo<SysAlloc>(memtx);
+	sys_alloc_destroy(&memtx->sys_alloc);
 	xdir_destroy(&memtx->snap_dir);
 	free(memtx);
 }
@@ -1071,11 +1077,14 @@ memtx_engine_memory_stat(struct engine *engine, struct engine_memory_stat *stat)
 {
 	struct memtx_engine *memtx = (struct memtx_engine *)engine;
 	struct small_stats data_stats;
+	struct sys_stats malloc_stats;
 	struct mempool_stats index_stats;
 	mempool_stats(&memtx->index_extent_pool, &index_stats);
 	small_stats(&memtx->alloc, &data_stats, small_stats_noop_cb, NULL);
 	stat->data += data_stats.used;
 	stat->index += index_stats.totals.used;
+	sys_stats(&memtx->sys_alloc, &malloc_stats);
+	stat->data += malloc_stats.used;
 }
 
 static const struct engine_vtab memtx_engine_vtab = {
@@ -1151,7 +1160,8 @@ memtx_engine_gc_f(va_list va)
 struct memtx_engine *
 memtx_engine_new(const char *snap_dirname, bool force_recovery,
 		 uint64_t tuple_arena_max_size, uint32_t objsize_min,
-		 bool dontdump, unsigned granularity, float alloc_factor)
+		 bool dontdump, unsigned granularity,
+		 const char *allocator, float alloc_factor)
 {
 	int64_t snap_signature;
 	struct memtx_engine *memtx =
@@ -1224,10 +1234,15 @@ memtx_engine_new(const char *snap_dirname, bool force_recovery,
 	small_alloc_create(&memtx->alloc, &memtx->slab_cache,
 			   objsize_min, granularity, alloc_factor,
 			   &actual_alloc_factor);
-	create_memtx_tuple_format_vtab<SmallAlloc>(&memtx_tuple_format_vtab);
+	sys_alloc_create(&memtx->sys_alloc, &memtx->quota);
+	if (!strcmp(allocator, "small"))
+		create_memtx_tuple_format_vtab<SmallAlloc>(&memtx_tuple_format_vtab);
+	else
+		create_memtx_tuple_format_vtab<SysAlloc>(&memtx_tuple_format_vtab);
 	say_info("Actual slab_alloc_factor calculated on the basis of desired "
 		 "slab_alloc_factor = %f", actual_alloc_factor);
 	lifo_init(&memtx->delayed);
+	lifo_init(&memtx->sys_delayed);
 	memtx->free_mode = MEMTX_ENGINE_FREE;
 
 	/* Initialize index extent allocator. */

tsafin/tarantool-dsql

src/box/memtx_engine.cc
@@ -59,6 +59,7 @@
 #define SNAP_SYNC_INTERVAL	(1 << 24)
 
 struct small_alloc SmallAlloc::small_alloc;
+struct sys_alloc SysAlloc::sys_alloc;
 
 static void
 checkpoint_cancel(struct checkpoint *ckpt);
@@ -94,6 +95,8 @@ memtx_engine_get_delayed_lifo(struct memtx_engine *memtx)
 {
 	if (std::is_same<ALLOC, SmallAlloc>::value)
 		return &memtx->delayed;
+	else if (std::is_same<ALLOC, SysAlloc>::value)
+		return &memtx->sys_delayed;
 	unreachable();
 	return NULL;
 }
@@ -222,6 +225,8 @@ memtx_engine_shutdown(struct engine *engine)
 	SmallAlloc::destroy();
 	slab_cache_destroy(&memtx->slab_cache);
 	tuple_arena_destroy(&memtx->arena);
+	memtx_free_all_garbage_tuples<SysAlloc>(memtx);
+	SysAlloc::destroy();
 	xdir_destroy(&memtx->snap_dir);
 	free(memtx);
 }
@@ -1102,6 +1107,9 @@ memtx_engine_memory_stat(struct engine *engine, struct engine_memory_stat *stat)
 	SmallAlloc::stats(&data_stats);
 	stat->data += data_stats.used;
 	stat->index += index_stats.totals.used;
+	memset(&data_stats, 0, sizeof(data_stats));
+	SysAlloc::stats(&data_stats);
+	stat->data += data_stats.used;
 }
 
 static const struct engine_vtab memtx_engine_vtab = {
@@ -1177,7 +1185,8 @@ memtx_engine_gc_f(va_list va)
 struct memtx_engine *
 memtx_engine_new(const char *snap_dirname, bool force_recovery,
 		 uint64_t tuple_arena_max_size, uint32_t objsize_min,
-		 bool dontdump, unsigned granularity, float alloc_factor)
+		 bool dontdump, unsigned granularity,
+		 const char *allocator, float alloc_factor)
 {
 	int64_t snap_signature;
 	struct memtx_engine *memtx =
@@ -1249,10 +1258,15 @@ memtx_engine_new(const char *snap_dirname, bool force_recovery,
 	float actual_alloc_factor;
 	SmallAlloc::create(&memtx->slab_cache, objsize_min, granularity,
 			  alloc_factor, &actual_alloc_factor);
-	create_memtx_tuple_format_vtab<SmallAlloc>(&memtx_tuple_format_vtab);
+	SysAlloc::create(&memtx->quota);
+	if (!strcmp(allocator, "small"))
+		create_memtx_tuple_format_vtab<SmallAlloc>(&memtx_tuple_format_vtab);
+	else
+		create_memtx_tuple_format_vtab<SysAlloc>(&memtx_tuple_format_vtab);
 	say_info("Actual slab_alloc_factor calculated on the basis of desired "
 		 "slab_alloc_factor = %f", actual_alloc_factor);
 	lifo_init(&memtx->delayed);
+	lifo_init(&memtx->sys_delayed);
 	memtx->free_mode = MEMTX_ENGINE_FREE;
 
 	/* Initialize index extent allocator. */