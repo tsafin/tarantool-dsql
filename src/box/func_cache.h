alyapunov/tarantool

box: add pin/unping infrastructure for func cache
There are cases when we need to be sure that a function is not
deleted and/or removed from func cache. For example constraints:
they must hold a pointer to struct func while it's very hard to
determine whether there'a constraint that points to given func.

Implement func pin/unpin for this purpose. You can pin a func to
declare that the func must not be deleted. To have to unpin it
when the func is not needed anymore.

NO_DOC=refactoring
NO_CHANGELOG=refactoring

src/box/func_cache.h
@@ -15,6 +15,36 @@ extern "C" {
 struct func;
 
 /**
+ * Type of a holder that can pin a func. @sa struct func_cache_holder.
+ */
+enum func_cache_holder_type {
+	FUNC_HOLDER_CONSTRAINT,
+	FUNC_HOLDER_MAX,
+};
+
+/**
+ * Lowercase name of each type.
+ */
+extern const char *func_cache_holder_type_strs[FUNC_HOLDER_MAX];
+
+/**
+ * Definition of a holder that pinned some func. Pinning of a func is
+ * a mechanism that is designed for preventing of deletion of some func from
+ * func cache by storing links to holders that prevented that.
+ */
+struct func_cache_holder {
+	/** Holders of the same func are linked into ring list by this link. */
+	struct rlist link;
+	/** Actual pointer to func. */
+	struct func *func;
+	/**
+	 * Type of holder, mostly for better error generation, but also can be
+	 * used for proper container_of application.
+	 */
+	enum func_cache_holder_type type;
+};
+
+/**
  * Initialize function cache storage.
  */
 void
@@ -35,6 +65,9 @@ func_cache_insert(struct func *func);
 
 /**
  * Delete a function object from the function cache.
+ * The function must not have any keepers (assert, @sa func_cache_is_kept),
+ * so if there is no assurance that there are no pins, @sa func_cache_is_pinned
+ * must be called before.
  * If the function is not found by five ID - do nothing.
  * @param fid ID of function object.
  */
@@ -55,6 +88,33 @@ func_by_id(uint32_t fid);
  */
 struct func *
 func_by_name(const char *name, uint32_t name_len);
+
+/**
+ * Register that there is a @a holder of type @a type that is dependent
+ * on function @a func.
+ * The function must be in cache (asserted).
+ * If a function has holders, it must not be deleted (asserted).
+ */
+void
+func_cache_pin(struct func *func, struct func_cache_holder *holder,
+	       enum func_cache_holder_type type);
+
+/**
+ * Notify that a @a holder does not depend anymore on function.
+ * The function must be in cache (asserted).
+ * If a function has no holders, it can be deleted.
+ */
+void
+func_cache_unpin( struct func_cache_holder *holder);
+
+/**
+ * Check whether the function @a func has holders or not.
+ * If it has, @a type argument is set to the first holder's type.
+ * The function must be in cache (asserted).
+ * If a function has holders, it must not be deleted (asserted).
+ */
+bool
+func_cache_is_pinned(struct func *func, enum func_cache_holder_type *type);
 
 #if defined(__cplusplus)
 } /* extern "C" */

tsafin/tarantool-dsql

src/box/func_cache.h
@@ -15,6 +15,36 @@ extern "C" {
 struct func;
 
 /**
+ * Type of a holder that can pin a func. @sa struct func_cache_holder.
+ */
+enum func_holder_type {
+	FUNC_HOLDER_CONSTRAINT,
+	FUNC_HOLDER_MAX,
+};
+
+/**
+ * Lowercase name of each type.
+ */
+extern const char *func_cache_holder_type_strs[FUNC_HOLDER_MAX];
+
+/**
+ * Definition of a holder that pinned some func. Pinning of a func is
+ * a mechanism that is designed for preventing of deletion of some func from
+ * func cache by storing links to holders that prevented that.
+ */
+struct func_cache_holder {
+	/** Holders of the same func are linked into ring list by this link. */
+	struct rlist link;
+	/** Actual pointer to func. */
+	struct func *func;
+	/**
+	 * Type of holder, mostly for better error generation, but also can be
+	 * used for proper container_of application.
+	 */
+	enum func_holder_type type;
+};
+
+/**
  * Initialize function cache storage.
  */
 void
@@ -35,6 +65,9 @@ func_cache_insert(struct func *func);
 
 /**
  * Delete a function object from the function cache.
+ * The function must not have any keepers (assert, @sa func_cache_is_kept),
+ * so if there is no assurance that there are no pins, @sa func_cache_is_pinned
+ * must be called before.
  * If the function is not found by five ID - do nothing.
  * @param fid ID of function object.
  */
@@ -55,6 +88,33 @@ func_by_id(uint32_t fid);
  */
 struct func *
 func_by_name(const char *name, uint32_t name_len);
+
+/**
+ * Register that there is a @a holder of type @a type that is dependent
+ * on function @a func.
+ * The function must be in cache (asserted).
+ * If a function has holders, it must not be deleted (asserted).
+ */
+void
+func_pin(struct func *func, struct func_cache_holder *holder,
+	 enum func_holder_type type);
+
+/**
+ * Notify that a @a holder does not depend anymore on function.
+ * The function must be in cache (asserted).
+ * If a function has no holders, it can be deleted.
+ */
+void
+func_unpin(struct func_cache_holder *holder);
+
+/**
+ * Check whether the function @a func has holders or not.
+ * If it has, @a type argument is set to the first holder's type.
+ * The function must be in cache (asserted).
+ * If a function has holders, it must not be deleted (asserted).
+ */
+bool
+func_is_pinned(struct func *func, enum func_holder_type *type);
 
 #if defined(__cplusplus)
 } /* extern "C" */