alyapunov/tarantool

box: implement complex foreign keys
Implement complext foreign keys addition to field foreign keys.
They are quite similar to field foreign keys, the difference is:
* The are set in space options instead of format field definition.
* Several fields may be specified in relation.
* By design field foreign keys are more optimal.

One can set up foreign keys in space options:
box.schema.space.create(.. {.., foreign_key=<foreign_key>})
where foreign_key can be of one of the following forms:
 foreign_key={space=..,field=..}
 foreign_key={<name1>={space=..,field=..}, ..}
where field must be a table with local -> foreing fields mapping:
 field={local_field1=foreign_field1, ..}

NO_DOC=see later commits
NO_CHANGELOG=see later commits

src/box/lua/schema.lua
@@ -448,7 +448,11 @@ end
 
 -- Helper of prepare_foreign_key.
 -- Check and normalize one foreign key definition.
-local function prepare_foreign_key_one(def, error_prefix)
+-- If not is_complex, field is expected to be a numeric ID or string name of
+--  foreign field.
+-- If is_complex, field is expected to be a table with local field ->
+--  foreign field mapping.
+local function prepare_foreign_key_one(def, error_prefix, is_complex)
     if def.space == nil then
         box.error(box.error.ILLEGAL_PARAMS,
                   error_prefix .. "foreign key: space must be specified")
@@ -461,13 +465,51 @@ local function prepare_foreign_key_one(def, error_prefix)
         box.error(box.error.ILLEGAL_PARAMS,
                   error_prefix .. "foreign key: space must be string or number")
     end
-    if type(def.field) ~= 'string' and type(def.field) ~= 'number' then
-        box.error(box.error.ILLEGAL_PARAMS,
-                  error_prefix .. "foreign key: field must be string or number")
-    end
-    if type(def.field) == 'number' then
-        -- convert to zero-based index.
-        def.field = def.field - 1
+    if not is_complex then
+        if type(def.field) ~= 'string' and type(def.field) ~= 'number' then
+            box.error(box.error.ILLEGAL_PARAMS,
+                      error_prefix .. "foreign key: field must be string or number")
+        end
+        if type(def.field) == 'number' then
+            -- convert to zero-based index.
+            def.field = def.field - 1
+        end
+    else
+        if type(def.field) ~= 'table' then
+            box.error(box.error.ILLEGAL_PARAMS,
+                      error_prefix .. "foreign key: field must be a table " ..
+                      "with local field -> foreign field mapping")
+        end
+        local count = 0
+        local converted = {}
+        for k,v in pairs(def.field) do
+            count = count + 1
+            if type(k) ~= 'string' and type(k) ~= 'number' then
+                box.error(box.error.ILLEGAL_PARAMS,
+                          error_prefix .. "foreign key: local field must be "
+                          .. "string or number")
+            end
+            if type(k) == 'number' then
+                -- convert to zero-based index.
+                k = k - 1
+            end
+            if type(v) ~= 'string' and type(v) ~= 'number' then
+                box.error(box.error.ILLEGAL_PARAMS,
+                          error_prefix .. "foreign key: foreign field must be "
+                          .. "string or number")
+            end
+            if type(v) == 'number' then
+                -- convert to zero-based index.
+                v = v - 1
+            end
+            converted[k] = v
+        end
+        if count < 1 then
+            box.error(box.error.ILLEGAL_PARAMS,
+                      error_prefix .. "foreign key: field must be a table " ..
+                      "with local field -> foreign field mapping")
+        end
+        def.field = setmap(converted)
     end
     if not box.space[def.space] then
         box.error(box.error.ILLEGAL_PARAMS,
@@ -488,9 +530,13 @@ end
 -- Given definition @a fkey is expected to be one of:
 -- {space=.., field=..}
 -- {fkey_name={space=.., field=..}, }
+-- If not is_complex, field is expected to be a numeric ID or string name of
+--  foreign field.
+-- If is_complex, field is expected to be a table with local field ->
+--  foreign field mapping.
 -- In case of error box.error.ILLEGAL_PARAMS is raised, and @a error_prefix
 --  is added before string message.
-local function prepare_foreign_key(fkey, error_prefix)
+local function prepare_foreign_key(fkey, error_prefix, is_complex)
     if fkey == nil then
         return nil
     end
@@ -502,7 +548,7 @@ local function prepare_foreign_key(fkey, error_prefix)
     if fkey.space ~= nil and fkey.field ~= nil and
         (type(fkey.space) ~= 'table' or type(fkey.field) ~= 'table') then
         -- the first, short form.
-        fkey = prepare_foreign_key_one(fkey, error_prefix)
+        fkey = prepare_foreign_key_one(fkey, error_prefix, is_complex)
         return {[box.space[fkey.space].name]=fkey}
     end
     -- the second, detailed form.
@@ -518,7 +564,7 @@ local function prepare_foreign_key(fkey, error_prefix)
                       error_prefix .. "foreign key definition must be a table "
                       .. "with 'space' and 'field' members")
         end
-        v = prepare_foreign_key_one(v, error_prefix)
+        v = prepare_foreign_key_one(v, error_prefix, is_complex)
         result[k] = v
     end
     return result
@@ -590,6 +636,7 @@ box.schema.space.create = function(name, options)
         temporary = 'boolean',
         is_sync = 'boolean',
         constraint = 'string, table',
+        foreign_key = 'table',
     }
     local options_defaults = {
         engine = 'memtx',
@@ -631,12 +678,14 @@ box.schema.space.create = function(name, options)
     check_param(format, 'format', 'table')
     format = update_format(format)
     local constraint = prepare_constraint(options.constraint, '')
+    local foreign_key = prepare_foreign_key(options.foreign_key, '', true)
     -- filter out global parameters from the options array
     local space_options = setmap({
         group_id = options.is_local and 1 or nil,
         temporary = options.temporary and true or nil,
         is_sync = options.is_sync,
         constraint = constraint,
+        foreign_key = foreign_key,
     })
     _space:insert{id, uid, name, options.engine, options.field_count,
         space_options, format}
@@ -731,6 +780,7 @@ local alter_space_template = {
     is_sync = 'boolean',
     name = 'string',
     constraint = 'string, table',
+    foreign_key = 'table',
 }
 
 box.schema.space.alter = function(space_id, options)
@@ -778,6 +828,13 @@ box.schema.space.alter = function(space_id, options)
             options.constraint = nil
         end
         flags.constraint = prepare_constraint(options.constraint, '')
+    end
+
+    if options.foreign_key ~= nil then
+        if table.equals(options.foreign_key, {}) then
+            options.foreign_key = nil
+        end
+        flags.foreign_key = prepare_foreign_key(options.foreign_key, '', true)
     end
 
     tuple = tuple:totable()

src/box/tuple_constraint_def.c
@@ -35,7 +35,23 @@ tuple_constraint_def_cmp_fkey(const struct tuple_constraint_fkey_def *def1,
 {
 	if (def1->space_id != def2->space_id)
 		return def1->space_id < def2->space_id ? -1 : 1;
-	return id_or_name_def_cmp(&def1->field, &def2->field);
+	if (def1->field_mapping_size != def2->field_mapping_size)
+		return def1->field_mapping_size < def2->field_mapping_size ? -1 : 1;
+	if (def1->field_mapping_size == 0)
+		return id_or_name_def_cmp(&def1->field, &def2->field);
+
+	for (uint32_t i = 0; i < def1->field_mapping_size; i++) {
+		int rc;
+		rc = id_or_name_def_cmp(&def1->field_mapping[i].local_field,
+					&def2->field_mapping[i].local_field);
+		if (rc != 0)
+			return rc;
+		rc = id_or_name_def_cmp(&def1->field_mapping[i].foreign_field,
+					&def2->field_mapping[i].foreign_field);
+		if (rc != 0)
+			return rc;
+	}
+	return 0;
 }
 
 int
@@ -70,6 +86,22 @@ id_or_name_def_hash_process(const struct tuple_constraint_id_or_name_def *def,
 	return sizeof(def->id) + sizeof(def->name_len) + def->name_len;
 }
 
+static uint32_t
+field_mapping_hash_process(const struct tuple_constraint_fkey_def *def,
+			   uint32_t *ph, uint32_t *pcarry)
+{
+	uint32_t bytes = 0;
+	for (uint32_t i = 0; i < def->field_mapping_size; i++) {
+		struct tuple_constraint_fkey_field_mapping *m =
+			&def->field_mapping[i];
+		bytes += id_or_name_def_hash_process(&m->local_field,
+						     ph, pcarry);
+		bytes += id_or_name_def_hash_process(&m->foreign_field,
+						     ph, pcarry);
+	}
+	return bytes;
+}
+
 uint32_t
 tuple_constraint_def_hash_process(const struct tuple_constraint_def *def,
 				  uint32_t *ph, uint32_t *pcarry)
@@ -86,8 +118,15 @@ tuple_constraint_def_hash_process(const struct tuple_constraint_def *def,
 		PMurHash32_Process(ph, pcarry, &def->fkey.space_id,
 				   sizeof(def->fkey.space_id));
 		bytes += sizeof(def->fkey.space_id);
-		bytes += id_or_name_def_hash_process(&def->fkey.field,
-						     ph, pcarry);
+		PMurHash32_Process(ph, pcarry, &def->fkey.field_mapping_size,
+				   sizeof(def->fkey.field_mapping_size));
+		bytes += sizeof(def->fkey.field_mapping_size);
+		if (def->fkey.field_mapping_size == 0)
+			bytes += id_or_name_def_hash_process(&def->fkey.field,
+							     ph, pcarry);
+		else
+			bytes += field_mapping_hash_process(&def->fkey,
+							     ph, pcarry);
 	}
 	return bytes;
 }
@@ -190,11 +229,49 @@ id_or_name_def_decode(const char **data,
 	return 0;
 }
 
+/**
+ * Helper function of tuple_constraint_def_decode_fkey.
+ * Decode foreign key field mapping, that is expected to be MP_MAP with
+ * local field (id or name) -> foreign field (id or name) correspondence.
+ */
+static int
+field_mapping_decode(const char **data,
+		     struct tuple_constraint_fkey_def *fkey,
+		     struct region *region, const char **error)
+{
+	if (mp_typeof(**data) != MP_MAP) {
+		*error = "field mapping is expected to be a map";
+		return -1;
+	}
+	uint32_t mapping_size = mp_decode_map(data);
+	if (mapping_size == 0) {
+		*error = "field mapping is expected to be a map";
+		return -1;
+	}
+	fkey->field_mapping_size = mapping_size;
+	size_t sz;
+	fkey->field_mapping = region_alloc_array(region,
+		struct tuple_constraint_fkey_field_mapping, mapping_size, &sz);
+	if (fkey->field_mapping == NULL) {
+		*error = "field mapping";
+		return sz;
+	}
+	for (uint32_t i = 0 ; i < 2 * mapping_size; i++) {
+		struct tuple_constraint_id_or_name_def *def = i % 2 == 0 ?
+			&fkey->field_mapping[i / 2].local_field :
+			&fkey->field_mapping[i / 2].foreign_field;
+		int rc = id_or_name_def_decode(data, def, region, error);
+		if (rc != 0)
+			return rc;
+	}
+	return 0;
+}
+
 int
 tuple_constraint_def_decode_fkey(const char **data,
 				 struct tuple_constraint_def **def,
-				 uint32_t *count,
-				 struct region *region, const char **error)
+				 uint32_t *count, struct region *region,
+				 const char **error, bool is_complex)
 {
 	/*
 	 * Expected normal form of foreign keys: {name1=data1, name2=data2..},
@@ -245,6 +322,7 @@ tuple_constraint_def_decode_fkey(const char **data,
 				 "is expected to be a map";
 			return -1;
 		}
+		new_def[i].fkey.field_mapping_size = 0;
 		uint32_t def_size = mp_decode_map(data);
 		bool has_space = false, has_field = false;
 		for (size_t j = 0; j < def_size; j++) {
@@ -271,13 +349,15 @@ tuple_constraint_def_decode_fkey(const char **data,
 			}
 			int rc;
 			struct tuple_constraint_fkey_def *fk = &new_def[i].fkey;
-			if (is_space) {
+			if (is_space)
 				rc = space_id_decode(data, &fk->space_id,
 						     error);
-			} else {
+			else if (!is_complex)
 				rc = id_or_name_def_decode(data, &fk->field,
 							   region, error);
-			}
+			else
+				rc = field_mapping_decode(data, fk,
+							  region, error);
 			if (rc != 0)
 				return rc;
 		}
@@ -288,6 +368,15 @@ tuple_constraint_def_decode_fkey(const char **data,
 		}
 	}
 	return 0;
+}
+
+static void
+id_or_name_def_reserve_bank(const struct tuple_constraint_id_or_name_def *def,
+			    struct data_bank *bank)
+{
+	/* Reservation is required only for strings. */
+	if (def->name_len != 0)
+		data_bank_reserve_str(bank, def->name_len);
 }
 
 static void
@@ -303,6 +392,40 @@ id_or_name_def_copy(struct tuple_constraint_id_or_name_def *dst,
 		dst->name = "";
 }
 
+static void
+field_mapping_reserve_bank(const struct tuple_constraint_fkey_def *def,
+			   struct data_bank *bank)
+{
+	assert(def->field_mapping_size != 0);
+	size_t bytes = def->field_mapping_size * sizeof(def->field_mapping[0]);
+	data_bank_reserve_data(bank, bytes);
+	for (uint32_t i = 0; i < def->field_mapping_size; i++) {
+		const struct tuple_constraint_fkey_field_mapping *f =
+			&def->field_mapping[i];
+		id_or_name_def_reserve_bank(&f->local_field, bank);
+		id_or_name_def_reserve_bank(&f->foreign_field, bank);
+	}
+}
+
+static void
+field_mapping_copy(struct tuple_constraint_fkey_def *dst,
+		   const struct tuple_constraint_fkey_def *src,
+		   struct data_bank *bank)
+{
+	assert(src->field_mapping_size != 0);
+	dst->field_mapping_size = src->field_mapping_size;
+	size_t bytes = src->field_mapping_size * sizeof(dst->field_mapping[0]);
+	dst->field_mapping = data_bank_create_data(bank, bytes);
+	for (uint32_t i = 0; i < src->field_mapping_size; i++) {
+		struct tuple_constraint_fkey_field_mapping *d =
+			&dst->field_mapping[i];
+		const struct tuple_constraint_fkey_field_mapping *s =
+			&src->field_mapping[i];
+		id_or_name_def_copy(&d->local_field, &s->local_field, bank);
+		id_or_name_def_copy(&d->foreign_field, &s->foreign_field, bank);
+	}
+}
+
 /**
  * Reserve strings needed for given constraint definition @a dev in given
  * string @a bank.
@@ -315,8 +438,10 @@ tuple_constraint_def_reserve_bank(const struct tuple_constraint_def *def,
 	if (def->type == CONSTR_FUNC) {
 	} else {
 		assert(def->type == CONSTR_FKEY);
-		if (def->fkey.field.name_len != 0)
-			data_bank_reserve_str(bank, def->fkey.field.name_len);
+		if (def->fkey.field_mapping_size == 0)
+			id_or_name_def_reserve_bank(&def->fkey.field, bank);
+		else
+			field_mapping_reserve_bank(&def->fkey, bank);
 	}
 }
 
@@ -337,7 +462,12 @@ tuple_constraint_def_copy(struct tuple_constraint_def *dst,
 	} else {
 		assert(src->type == CONSTR_FKEY);
 		dst->fkey.space_id = src->fkey.space_id;
-		id_or_name_def_copy(&dst->fkey.field, &src->fkey.field, bank);
+		dst->fkey.field_mapping_size = 0;
+		if (src->fkey.field_mapping_size == 0)
+			id_or_name_def_copy(&dst->fkey.field, &src->fkey.field,
+					    bank);
+		else
+			field_mapping_copy(&dst->fkey, &src->fkey, bank);
 	}
 }
 

tsafin/tarantool-dsql

src/box/lua/schema.lua
@@ -453,7 +453,11 @@ end
 
 -- Helper of normalize_foreign_key.
 -- Check and normalize one foreign key definition.
-local function normalize_foreign_key_one(def, error_prefix)
+-- If not is_complex, field is expected to be a numeric ID or string name of
+--  foreign field.
+-- If is_complex, field is expected to be a table with local field ->
+--  foreign field mapping.
+local function normalize_foreign_key_one(def, error_prefix, is_complex)
     if def.space == nil then
         box.error(box.error.ILLEGAL_PARAMS,
                   error_prefix .. "foreign key: space must be specified")
@@ -466,13 +470,51 @@ local function normalize_foreign_key_one(def, error_prefix)
         box.error(box.error.ILLEGAL_PARAMS,
                   error_prefix .. "foreign key: space must be string or number")
     end
-    if type(def.field) ~= 'string' and type(def.field) ~= 'number' then
-        box.error(box.error.ILLEGAL_PARAMS,
-                  error_prefix .. "foreign key: field must be string or number")
-    end
-    if type(def.field) == 'number' then
-        -- convert to zero-based index.
-        def.field = def.field - 1
+    if not is_complex then
+        if type(def.field) ~= 'string' and type(def.field) ~= 'number' then
+            box.error(box.error.ILLEGAL_PARAMS,
+                      error_prefix .. "foreign key: field must be string or number")
+        end
+        if type(def.field) == 'number' then
+            -- convert to zero-based index.
+            def.field = def.field - 1
+        end
+    else
+        if type(def.field) ~= 'table' then
+            box.error(box.error.ILLEGAL_PARAMS,
+                      error_prefix .. "foreign key: field must be a table " ..
+                      "with local field -> foreign field mapping")
+        end
+        local count = 0
+        local converted = {}
+        for k,v in pairs(def.field) do
+            count = count + 1
+            if type(k) ~= 'string' and type(k) ~= 'number' then
+                box.error(box.error.ILLEGAL_PARAMS,
+                          error_prefix .. "foreign key: local field must be "
+                          .. "string or number")
+            end
+            if type(k) == 'number' then
+                -- convert to zero-based index.
+                k = k - 1
+            end
+            if type(v) ~= 'string' and type(v) ~= 'number' then
+                box.error(box.error.ILLEGAL_PARAMS,
+                          error_prefix .. "foreign key: foreign field must be "
+                          .. "string or number")
+            end
+            if type(v) == 'number' then
+                -- convert to zero-based index.
+                v = v - 1
+            end
+            converted[k] = v
+        end
+        if count < 1 then
+            box.error(box.error.ILLEGAL_PARAMS,
+                      error_prefix .. "foreign key: field must be a table " ..
+                      "with local field -> foreign field mapping")
+        end
+        def.field = setmap(converted)
     end
     if not box.space[def.space] then
         box.error(box.error.ILLEGAL_PARAMS,
@@ -493,9 +535,13 @@ end
 -- Given definition @a fkey is expected to be one of:
 -- {space=.., field=..}
 -- {fkey_name={space=.., field=..}, }
+-- If not is_complex, field is expected to be a numeric ID or string name of
+--  foreign field.
+-- If is_complex, field is expected to be a table with local field ->
+--  foreign field mapping.
 -- In case of error box.error.ILLEGAL_PARAMS is raised, and @a error_prefix
 --  is added before string message.
-local function normalize_foreign_key(fkey, error_prefix)
+local function normalize_foreign_key(fkey, error_prefix, is_complex)
     if fkey == nil then
         return nil
     end
@@ -507,7 +553,7 @@ local function normalize_foreign_key(fkey, error_prefix)
     if fkey.space ~= nil and fkey.field ~= nil and
         (type(fkey.space) ~= 'table' or type(fkey.field) ~= 'table') then
         -- the first, short form.
-        fkey = normalize_foreign_key_one(fkey, error_prefix)
+        fkey = normalize_foreign_key_one(fkey, error_prefix, is_complex)
         return {[box.space[fkey.space].name]=fkey}
     end
     -- the second, detailed form.
@@ -523,7 +569,7 @@ local function normalize_foreign_key(fkey, error_prefix)
                       error_prefix .. "foreign key definition must be a table "
                       .. "with 'space' and 'field' members")
         end
-        v = normalize_foreign_key_one(v, error_prefix)
+        v = normalize_foreign_key_one(v, error_prefix, is_complex)
         result[k] = v
     end
     return result
@@ -596,6 +642,7 @@ box.schema.space.create = function(name, options)
         is_sync = 'boolean',
         defer_deletes = 'boolean',
         constraint = 'string, table',
+        foreign_key = 'table',
     }
     local options_defaults = {
         engine = 'memtx',
@@ -642,6 +689,7 @@ box.schema.space.create = function(name, options)
     check_param(format, 'format', 'table')
     format = update_format(format)
     local constraint = normalize_constraint(options.constraint, '')
+    local foreign_key = normalize_foreign_key(options.foreign_key, '', true)
     -- filter out global parameters from the options array
     local space_options = setmap({
         group_id = options.is_local and 1 or nil,
@@ -649,6 +697,7 @@ box.schema.space.create = function(name, options)
         is_sync = options.is_sync,
         defer_deletes = options.defer_deletes and true or nil,
         constraint = constraint,
+        foreign_key = foreign_key,
     })
     _space:insert{id, uid, name, options.engine, options.field_count,
         space_options, format}
@@ -744,6 +793,7 @@ local alter_space_template = {
     defer_deletes = 'boolean',
     name = 'string',
     constraint = 'string, table',
+    foreign_key = 'table',
 }
 
 box.schema.space.alter = function(space_id, options)
@@ -795,6 +845,13 @@ box.schema.space.alter = function(space_id, options)
             options.constraint = nil
         end
         flags.constraint = normalize_constraint(options.constraint, '')
+    end
+
+    if options.foreign_key ~= nil then
+        if table.equals(options.foreign_key, {}) then
+            options.foreign_key = nil
+        end
+        flags.foreign_key = normalize_foreign_key(options.foreign_key, '', true)
     end
 
     tuple = tuple:totable()

src/box/tuple_constraint_def.c
@@ -35,7 +35,24 @@ tuple_constraint_def_cmp_fkey(const struct tuple_constraint_fkey_def *def1,
 {
 	if (def1->space_id != def2->space_id)
 		return def1->space_id < def2->space_id ? -1 : 1;
-	return field_id_cmp(&def1->field, &def2->field);
+	if (def1->field_mapping_size != def2->field_mapping_size)
+		return def1->field_mapping_size < def2->field_mapping_size ? -1
+									   : 1;
+	if (def1->field_mapping_size == 0)
+		return field_id_cmp(&def1->field, &def2->field);
+
+	for (uint32_t i = 0; i < def1->field_mapping_size; i++) {
+		int rc;
+		rc = field_id_cmp(&def1->field_mapping[i].local_field,
+				  &def2->field_mapping[i].local_field);
+		if (rc != 0)
+			return rc;
+		rc = field_id_cmp(&def1->field_mapping[i].foreign_field,
+				  &def2->field_mapping[i].foreign_field);
+		if (rc != 0)
+			return rc;
+	}
+	return 0;
 }
 
 int
@@ -173,11 +190,54 @@ field_id_decode(const char **data, struct tuple_constraint_field_id *def,
 	return 0;
 }
 
+/**
+ * Helper function of tuple_constraint_def_decode_fkey.
+ * Decode foreign key field mapping, that is expected to be MP_MAP with
+ * local field (id or name) -> foreign field (id or name) correspondence.
+ */
+static int
+field_mapping_decode(const char **data,
+		     struct tuple_constraint_fkey_def *fkey,
+		     struct region *region, uint32_t errcode, uint32_t field_no)
+{
+	if (mp_typeof(**data) != MP_MAP) {
+		diag_set(ClientError, errcode, field_no,
+			 "field mapping is expected to be a map");
+		return -1;
+	}
+	uint32_t mapping_size = mp_decode_map(data);
+	if (mapping_size == 0) {
+		diag_set(ClientError, errcode, field_no,
+			 "field mapping is expected to be a map");
+		return -1;
+	}
+	fkey->field_mapping_size = mapping_size;
+	size_t sz;
+	fkey->field_mapping =
+		region_alloc_array(region,
+				   struct tuple_constraint_fkey_field_mapping,
+				   mapping_size, &sz);
+	if (fkey->field_mapping == NULL) {
+		diag_set(OutOfMemory, sz, "region", "field mapping");
+		return -1;
+	}
+	for (uint32_t i = 0 ; i < 2 * mapping_size; i++) {
+		struct tuple_constraint_field_id *def = i % 2 == 0 ?
+			&fkey->field_mapping[i / 2].local_field :
+			&fkey->field_mapping[i / 2].foreign_field;
+		int rc = field_id_decode(data, def, region, errcode, field_no);
+		if (rc != 0)
+			return rc;
+	}
+	return 0;
+}
+
 int
 tuple_constraint_def_decode_fkey(const char **data,
 				 struct tuple_constraint_def **def,
 				 uint32_t *count, struct region *region,
-				 uint32_t errcode, uint32_t field_no)
+				 uint32_t errcode, uint32_t field_no,
+				 bool is_complex)
 {
 	/*
 	 * Expected normal form of foreign keys: {name1=data1, name2=data2..},
@@ -232,6 +292,7 @@ tuple_constraint_def_decode_fkey(const char **data,
 				 "is expected to be a map");
 			return -1;
 		}
+		new_def[i].fkey.field_mapping_size = 0;
 		uint32_t def_size = mp_decode_map(data);
 		bool has_space = false, has_field = false;
 		for (size_t j = 0; j < def_size; j++) {
@@ -260,13 +321,16 @@ tuple_constraint_def_decode_fkey(const char **data,
 			}
 			int rc;
 			struct tuple_constraint_fkey_def *fk = &new_def[i].fkey;
-			if (is_space) {
+			if (is_space)
 				rc = space_id_decode(data, &fk->space_id,
 						     errcode, field_no);
-			} else {
+			else if (!is_complex)
 				rc = field_id_decode(data, &fk->field, region,
 						     errcode, field_no);
-			}
+			else
+				rc = field_mapping_decode(data, fk,
+							  region, errcode,
+							  field_no);
 			if (rc != 0)
 				return rc;
 		}
@@ -284,6 +348,15 @@ tuple_constraint_def_decode_fkey(const char **data,
  * Copy tuple_constraint_field_id object, allocating data on given allocator.
  */
 static void
+field_id_reserve(const struct tuple_constraint_field_id *def,
+		 struct grp_alloc *all)
+{
+	/* Reservation is required only for strings. */
+	if (def->name_len != 0)
+		grp_alloc_reserve_str(all, def->name_len);
+}
+
+static void
 field_id_copy(struct tuple_constraint_field_id *dst,
 	      const struct tuple_constraint_field_id *src,
 	      struct grp_alloc *all)
@@ -297,6 +370,47 @@ field_id_copy(struct tuple_constraint_field_id *dst,
 }
 
 /**
+ * Reserve memory for field mapping of given constraint definition @a def
+ * on an allocator @all.
+ */
+static void
+field_mapping_reserve(const struct tuple_constraint_fkey_def *def,
+		      struct grp_alloc *all)
+{
+	assert(def->field_mapping_size != 0);
+	size_t bytes = def->field_mapping_size * sizeof(def->field_mapping[0]);
+	grp_alloc_reserve_data(all, bytes);
+	for (uint32_t i = 0; i < def->field_mapping_size; i++) {
+		const struct tuple_constraint_fkey_field_mapping *f =
+			&def->field_mapping[i];
+		field_id_reserve(&f->local_field, all);
+		field_id_reserve(&f->foreign_field, all);
+	}
+}
+
+/**
+ * Copy field mapping array from one definition to another.
+ */
+static void
+field_mapping_copy(struct tuple_constraint_fkey_def *dst,
+		   const struct tuple_constraint_fkey_def *src,
+		   struct grp_alloc *all)
+{
+	assert(src->field_mapping_size != 0);
+	dst->field_mapping_size = src->field_mapping_size;
+	size_t bytes = src->field_mapping_size * sizeof(dst->field_mapping[0]);
+	dst->field_mapping = grp_alloc_create_data(all, bytes);
+	for (uint32_t i = 0; i < src->field_mapping_size; i++) {
+		struct tuple_constraint_fkey_field_mapping *d =
+			&dst->field_mapping[i];
+		const struct tuple_constraint_fkey_field_mapping *s =
+			&src->field_mapping[i];
+		field_id_copy(&d->local_field, &s->local_field, all);
+		field_id_copy(&d->foreign_field, &s->foreign_field, all);
+	}
+}
+
+/**
  * Reserve strings needed for given constraint definition @a dev in given
  * string @a bank.
  */
@@ -306,8 +420,10 @@ tuple_constraint_def_reserve(const struct tuple_constraint_def *def,
 {
 	grp_alloc_reserve_str(all, def->name_len);
 	if (def->type == CONSTR_FKEY) {
-		if (def->fkey.field.name_len != 0)
-			grp_alloc_reserve_str(all, def->fkey.field.name_len);
+		if (def->fkey.field_mapping_size == 0)
+			field_id_reserve(&def->fkey.field, all);
+		else
+			field_mapping_reserve(&def->fkey, all);
 	}
 }
 
@@ -328,7 +444,11 @@ tuple_constraint_def_copy(struct tuple_constraint_def *dst,
 	} else {
 		assert(src->type == CONSTR_FKEY);
 		dst->fkey.space_id = src->fkey.space_id;
-		field_id_copy(&dst->fkey.field, &src->fkey.field, all);
+		dst->fkey.field_mapping_size = 0;
+		if (src->fkey.field_mapping_size == 0)
+			field_id_copy(&dst->fkey.field, &src->fkey.field, all);
+		else
+			field_mapping_copy(&dst->fkey, &src->fkey, all);
 	}
 }
 