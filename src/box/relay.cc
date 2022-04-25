grafin/tarantool

replication: count bidirectional replica connections
Start counting replicas, whose connections are "healthy", e.g. there are
both relay and applier in SUBSCRIBE state.

Introduce triggers for the events of "healthy quorum" loss and gain.
"healthy quorum" is the same as replication_synchro_quorum.

The triggers will be used in raft pre-vote and synchro queue owner
fencing. Both these features require tracking whether there is a quorum
of nodes reachable from the local node.

Pre-requisite #6654
Pre-requisite #6661

NO_DOC=no user-visible changes
NO_CHANGELOG=no user-visible changes
NO_TEST=preparatory patch

src/box/relay.cc
@@ -979,6 +979,7 @@ relay_subscribe(struct replica *replica, struct iostream *io, uint64_t sync,
 
 	relay_start(relay, io, sync, relay_send_row,
 		    relay_yield_and_send_heartbeat);
+	replica_on_relay_start(replica);
 	auto relay_guard = make_scoped_guard([=] {
 		relay_stop(relay);
 		replica_on_relay_stop(replica);

tsafin/tarantool-dsql

src/box/relay.cc
@@ -1001,6 +1001,7 @@ relay_subscribe(struct replica *replica, struct iostream *io, uint64_t sync,
 
 	relay_start(relay, io, sync, relay_send_row,
 		    relay_yield_and_send_heartbeat);
+	replica_on_relay_follow(replica);
 	auto relay_guard = make_scoped_guard([=] {
 		relay_stop(relay);
 		replica_on_relay_stop(replica);