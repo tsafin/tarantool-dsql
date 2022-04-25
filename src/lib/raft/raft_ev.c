grafin/tarantool

raft: introduce Pre-Vote machinery
Support Pre-Vote in Raft: when a node knows, that some of the peers see
the leader of the current term, it doesn't start elections. Even when it
doesn't have a connection to the leader.

Don't use the machinery yet: nodes never share info on whether they see
the leader.

Part-of #6654

NO_DOC=no user-visible changes
NO_CHANGELOG=no user-visible changes

src/lib/raft/raft_ev.c
@@ -50,6 +50,12 @@ raft_ev_timer_stop(struct ev_loop *loop, struct ev_timer *watcher)
 	ev_timer_stop(loop, watcher);
 }
 
+double
+raft_ev_now(struct ev_loop *loop)
+{
+	return ev_now(loop);
+}
+
 struct ev_loop *
 raft_loop(void)
 {

src/lib/raft/raft_ev.h
@@ -50,6 +50,10 @@ raft_ev_timer_remaining(struct ev_loop *loop, struct ev_timer *watcher);
 void
 raft_ev_timer_stop(struct ev_loop *loop, struct ev_timer *watcher);
 
+/** A wrapper around ev_now(). */
+double
+raft_ev_now(struct ev_loop *loop);
+
 /** A wrapper around loop(), which returns this cord's ev_loop. */
 struct ev_loop *
 raft_loop(void);

tsafin/tarantool-dsql

src/lib/raft/raft_ev.c
@@ -50,6 +50,12 @@ raft_ev_timer_stop(struct ev_loop *loop, struct ev_timer *watcher)
 	ev_timer_stop(loop, watcher);
 }
 
+double
+raft_ev_monotonic_now(struct ev_loop *loop)
+{
+	return ev_monotonic_now(loop);
+}
+
 struct ev_loop *
 raft_loop(void)
 {

src/lib/raft/raft_ev.h
@@ -50,6 +50,10 @@ raft_ev_timer_remaining(struct ev_loop *loop, struct ev_timer *watcher);
 void
 raft_ev_timer_stop(struct ev_loop *loop, struct ev_timer *watcher);
 
+/** A wrapper around ev_monotonic_now(). */
+double
+raft_ev_monotonic_now(struct ev_loop *loop);
+
 /** A wrapper around loop(), which returns this cord's ev_loop. */
 struct ev_loop *
 raft_loop(void);