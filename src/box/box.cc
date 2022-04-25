Korablev77/tarantool

box: add OS stubs for flight recorder metrics
Let's add no-op interface functions and box cfg default parameters.

NO_DOC=<No user visible changes>
NO_TEST=<No functional changes>
NO_CHANGELOG=<No functional changes>

src/box/box.cc
@@ -773,7 +773,9 @@ box_check_flightrec(void)
 {
 	if (flightrec_check_cfg(cfg_geti64("flightrec_logs_size"),
 				cfg_geti64("flightrec_logs_max_msg_size"),
-				cfg_geti("flightrec_logs_log_level")) != 0)
+				cfg_geti("flightrec_logs_log_level"),
+				cfg_geti64("flightrec_metrics_interval"),
+				cfg_geti64("flightrec_metrics_period")) != 0)
 		diag_raise();
 }
 

src/box/flightrec.h
@@ -24,12 +24,15 @@ extern "C" {
 /** No-op in OS version. */
 static inline void
 flightrec_init(const char *fr_dirname, size_t logs_size,
-	       size_t logs_max_msg_size, int logs_log_level)
+	       size_t logs_max_msg_size, int logs_log_level,
+	       size_t metrics_interval, size_t metrics_period)
 {
 	(void)fr_dirname;
 	(void)logs_size;
 	(void)logs_max_msg_size;
 	(void)logs_log_level;
+	(void)metrics_interval;
+	(void)metrics_period;
 	say_error("Flight recorder is not available in this build");
 }
 
@@ -42,11 +45,14 @@ flightrec_free(void)
 /** No-op in OS version. */
 static inline int
 flightrec_check_cfg(int64_t logs_size, int64_t logs_max_msg_size,
-		    int logs_log_level)
+		    int logs_log_level, int64_t metrics_interval,
+		    int64_t metrics_period)
 {
 	(void)logs_size;
 	(void)logs_max_msg_size;
 	(void)logs_log_level;
+	(void)metrics_interval;
+	(void)metrics_period;
 	return 0;
 }
 

src/main.cc
@@ -471,7 +471,9 @@ load_cfg(void)
 		flightrec_init(cfg_gets("memtx_dir"),
 			       cfg_geti64("flightrec_logs_size"),
 			       cfg_geti64("flightrec_logs_max_msg_size"),
-			       cfg_geti("flightrec_logs_log_level"));
+			       cfg_geti("flightrec_logs_log_level"),
+			       cfg_geti("flightrec_metrics_interval"),
+			       cfg_geti("flightrec_metrics_period"));
 	}
 
 	memtx_tx_manager_use_mvcc_engine = cfg_getb("memtx_use_mvcc_engine");

tsafin/tarantool-dsql

src/box/box.cc
@@ -782,7 +782,9 @@ box_check_flightrec(void)
 	if (flightrec_check_cfg(
 			cfg_geti64("flightrec_logs_size"),
 			cfg_geti64("flightrec_logs_max_msg_size"),
-			cfg_geti("flightrec_logs_log_level")) != 0)
+			cfg_geti("flightrec_logs_log_level"),
+			cfg_getd("flightrec_metrics_interval"),
+			cfg_geti64("flightrec_metrics_period")) != 0)
 		diag_raise();
 }
 

src/box/flightrec.h
@@ -24,12 +24,15 @@ extern "C" {
 /** No-op in OS version. */
 static inline void
 flightrec_init(const char *fr_dirname, size_t logs_size,
-	       size_t logs_max_msg_size, int logs_log_level)
+	       size_t logs_max_msg_size, int logs_log_level,
+	       double metrics_interval, size_t metrics_period)
 {
 	(void)fr_dirname;
 	(void)logs_size;
 	(void)logs_max_msg_size;
 	(void)logs_log_level;
+	(void)metrics_interval;
+	(void)metrics_period;
 	say_error("Flight recorder is not available in this build");
 }
 
@@ -42,11 +45,14 @@ flightrec_free(void)
 /** No-op in OS version. */
 static inline int
 flightrec_check_cfg(int64_t logs_size, int64_t logs_max_msg_size,
-		    int logs_log_level)
+		    int logs_log_level, double metrics_interval,
+		    int64_t metrics_period)
 {
 	(void)logs_size;
 	(void)logs_max_msg_size;
 	(void)logs_log_level;
+	(void)metrics_interval;
+	(void)metrics_period;
 	return 0;
 }
 

src/main.cc
@@ -471,7 +471,9 @@ load_cfg(void)
 		flightrec_init(cfg_gets("memtx_dir"),
 			       cfg_geti64("flightrec_logs_size"),
 			       cfg_geti64("flightrec_logs_max_msg_size"),
-			       cfg_geti("flightrec_logs_log_level"));
+			       cfg_geti("flightrec_logs_log_level"),
+			       cfg_getd("flightrec_metrics_interval"),
+			       cfg_geti("flightrec_metrics_period"));
 	}
 
 	memtx_tx_manager_use_mvcc_engine = cfg_getb("memtx_use_mvcc_engine");