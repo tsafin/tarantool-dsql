ImeevMA/tarantool

box: do not expire SQL_EXPR functions
SQL_EXPR functions use the mechanics of prepared statements, so they can
expire after a schema change. However, since subselects are prohibited
in these functions, there is no point in expiring them. This patch makes
it so that SQL_EXPR functions no longer expire.

Part of #6986

NO_DOC=Bug
NO_CHANGELOG=Bug in new functionality

src/box/sql/func.c
@@ -2360,6 +2360,7 @@ func_sql_expr_new(const struct func_def *def)
 	}
 	int ref_reg = ++parser.nMem;
 	sqlVdbeAddOp2(v, OP_Variable, ++parser.nVar, ref_reg);
+	v->is_func = 1;
 	parser.vdbe_field_ref_reg = ref_reg;
 
 	sqlVdbeSetNumCols(v, 1);

src/box/sql/vdbeaux.c
@@ -2173,7 +2173,7 @@ sqlExpirePreparedStatements(sql * db)
 {
 	Vdbe *p;
 	for (p = db->pVdbe; p; p = p->pNext) {
-		p->expired = 1;
+		p->expired = p->is_func == 0 ? 1 : 0;
 	}
 }
 

tsafin/tarantool-dsql

src/box/sql/func.c
@@ -2385,6 +2385,7 @@ func_sql_expr_new(const struct func_def *def)
 	}
 	int ref_reg = ++parser.nMem;
 	sqlVdbeAddOp2(v, OP_Variable, ++parser.nVar, ref_reg);
+	v->is_sandboxed = 1;
 	parser.vdbe_field_ref_reg = ref_reg;
 
 	sqlVdbeSetNumCols(v, 1);

src/box/sql/vdbeaux.c
@@ -2165,7 +2165,7 @@ sqlExpirePreparedStatements(sql * db)
 {
 	Vdbe *p;
 	for (p = db->pVdbe; p; p = p->pNext) {
-		p->expired = 1;
+		p->expired = p->is_sandboxed == 0 ? 1 : 0;
 	}
 }
 