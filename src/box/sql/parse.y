tarantool/tarantool

sql: rework parsing of transaction statements
This patch reworks the parsing of statements associated with
transactions. There are six of them:
START TRANSACTION;
COMMIT;
ROLLBACK;
SAVEPOINT savepoint_name;
RELEASE [SAVEPOINT] savepoint_name;
ROLLBACK TO savepoint_name;

Prior to this patch, VDBE for these statements were generated while the
statement was in the process of being parsed. After this patch, VDBE for
all these statements will be generated only after the parsing has
successfully completed.

Part of #3319
Part of #4621
Part of #5485
Part of #8100

NO_DOC=Will be added later
NO_TEST=No user-visible changes are made
NO_CHANGELOG=Will be added later

src/box/sql/parse.y
@@ -108,10 +108,7 @@ static void disableLookaside(Parse *pParse){
 
 // Input is a single SQL command
 input ::= ecmd.
-ecmd ::= explain cmdx SEMI. {
-	if (!pParse->parse_only)
-		sql_finish_coding(pParse);
-}
+ecmd ::= explain cmdx SEMI.
 ecmd ::= SEMI. {
   diag_set(ClientError, ER_SQL_STATEMENT_EMPTY);
   pParse->is_aborted = true;
@@ -149,20 +146,26 @@ cmdx ::= cmd.
 ///////////////////// Begin and end transactions. ////////////////////////////
 //
 
-cmd ::= START TRANSACTION.  {sql_transaction_begin(pParse);}
-cmd ::= COMMIT.      {sql_transaction_commit(pParse);}
-cmd ::= ROLLBACK.    {sql_transaction_rollback(pParse);}
+cmd ::= START TRANSACTION. {
+  sql_parse_transaction_start(pParse);
+}
+cmd ::= COMMIT. {
+  sql_parse_transaction_commit(pParse);
+}
+cmd ::= ROLLBACK. {
+  sql_parse_transaction_rollback(pParse);
+}
 
 savepoint_opt ::= SAVEPOINT.
 savepoint_opt ::= .
 cmd ::= SAVEPOINT nm(X). {
-  sqlSavepoint(pParse, SAVEPOINT_BEGIN, &X);
+  sql_parse_savepoint_create(pParse, &X);
 }
 cmd ::= RELEASE savepoint_opt nm(X). {
-  sqlSavepoint(pParse, SAVEPOINT_RELEASE, &X);
+  sql_parse_savepoint_release(pParse, &X);
 }
 cmd ::= ROLLBACK TO savepoint_opt nm(X). {
-  sqlSavepoint(pParse, SAVEPOINT_ROLLBACK, &X);
+  sql_parse_savepoint_rollback(pParse, &X);
 }
 
 ///////////////////// The CREATE TABLE statement ////////////////////////////

tsafin/tarantool-dsql

src/box/sql/parse.y
@@ -108,10 +108,7 @@ static void disableLookaside(Parse *pParse){
 
 // Input is a single SQL command
 input ::= ecmd.
-ecmd ::= explain cmdx SEMI. {
-	if (!pParse->parse_only)
-		sql_finish_coding(pParse);
-}
+ecmd ::= explain cmdx SEMI.
 ecmd ::= SEMI. {
   diag_set(ClientError, ER_SQL_STATEMENT_EMPTY);
   pParse->is_aborted = true;
@@ -149,20 +146,26 @@ cmdx ::= cmd.
 ///////////////////// Begin and end transactions. ////////////////////////////
 //
 
-cmd ::= START TRANSACTION.  {sql_transaction_begin(pParse);}
-cmd ::= COMMIT.      {sql_transaction_commit(pParse);}
-cmd ::= ROLLBACK.    {sql_transaction_rollback(pParse);}
+cmd ::= START TRANSACTION. {
+  sql_ast_init_start_transaction(pParse);
+}
+cmd ::= COMMIT. {
+  sql_ast_init_commit(pParse);
+}
+cmd ::= ROLLBACK. {
+  sql_ast_init_rollback(pParse);
+}
 
 savepoint_opt ::= SAVEPOINT.
 savepoint_opt ::= .
 cmd ::= SAVEPOINT nm(X). {
-  sqlSavepoint(pParse, SAVEPOINT_BEGIN, &X);
+  sql_ast_init_savepoint(pParse, &X);
 }
 cmd ::= RELEASE savepoint_opt nm(X). {
-  sqlSavepoint(pParse, SAVEPOINT_RELEASE, &X);
+  sql_ast_init_release_savepoint(pParse, &X);
 }
 cmd ::= ROLLBACK TO savepoint_opt nm(X). {
-  sqlSavepoint(pParse, SAVEPOINT_ROLLBACK, &X);
+  sql_ast_init_rollback_to_savepoint(pParse, &X);
 }
 
 ///////////////////// The CREATE TABLE statement ////////////////////////////