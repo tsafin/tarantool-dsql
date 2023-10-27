alyapunov/tarantool

box: introduce `MP_TUPLE` extension
Add new MsgPack extension for tuples MP_TUPLE with the following structure:
+------+-----------------+----------+
|MP_EXT|      MP_UINT    | MP_ARRAY |
+------+-----------------+----------+
        ^                 ^
        format identifier tuple data

Needed for #8147

NO_CHANGELOG=<internal change>
NO_DOC=<internal change>

src/box/mp_tuple.c
@@ -1,0 +1,110 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 2010-2023, Tarantool AUTHORS, please see AUTHORS file.
+ */
+
+#include "box/mp_tuple.h"
+#include "box/tuple.h"
+#include "box/tuple_format_map.h"
+
+#include "core/mp_extension_types.h"
+
+#include "mpstream/mpstream.h"
+
+/** Length of packed tuple. */
+static uint32_t
+tuple_len(struct tuple *tuple)
+{
+	return mp_sizeof_uint(tuple->format_id) + tuple_bsize(tuple);
+}
+
+uint32_t
+mp_sizeof_tuple(struct tuple *tuple)
+{
+	return mp_sizeof_ext(tuple_len(tuple));
+}
+
+/** Pack a tuple value to a buffer. */
+static char *
+tuple_pack(char *data, struct tuple *tuple)
+{
+	data = mp_encode_uint(data, tuple->format_id);
+	uint32_t bsize = tuple_bsize(tuple);
+	memcpy(data, tuple_data(tuple), bsize);
+	return data + bsize;
+}
+
+struct tuple *
+mp_decode_tuple(const char **data, struct tuple_format_map *format_map)
+{
+	if (mp_typeof(**data) != MP_EXT)
+		return NULL;
+	int8_t type;
+	const char *svp = *data;
+	mp_decode_extl(data, &type);
+	if (type != MP_TUPLE)
+		goto error;
+	struct tuple *tuple = tuple_unpack(data, format_map);
+	if (tuple == NULL)
+		goto error;
+	return tuple;
+error:
+	*data = svp;
+	return NULL;
+}
+
+struct tuple *
+tuple_unpack(const char **data, struct tuple_format_map *format_map)
+{
+	uint32_t format_id = mp_decode_uint(data);
+	const char *tuple_data = *data;
+	mp_next(data);
+	struct tuple_format *format =
+		tuple_format_map_find(format_map, format_id);
+	if (format == NULL)
+		return NULL;
+	return tuple_new(format, tuple_data, *data);
+}
+
+char *
+mp_encode_tuple(char *data, struct tuple *tuple)
+{
+	data = mp_encode_extl(data, MP_TUPLE, tuple_len(tuple));
+	return tuple_pack(data, tuple);
+}
+
+void
+tuple_to_mpstream_as_ext(struct tuple *tuple, struct mpstream *stream)
+{
+	uint32_t tuple_sz = mp_sizeof_tuple(tuple);
+	char *data = mpstream_reserve(stream, tuple_sz);
+	if (data == NULL)
+		return;
+	char *pos = mp_encode_tuple(data, tuple);
+	mpstream_advance(stream, pos - data);
+}
+
+int
+mp_validate_tuple(const char *data, uint32_t len)
+{
+	/*
+	 * MsgPack extensions have length greater or equal than 1 by
+	 * specification.
+	 */
+	assert(len > 0);
+	const char *end = data + len;
+	enum mp_type type = mp_typeof(*data);
+	if (type == MP_UINT) {
+		if (mp_check_uint(data, end) > 0)
+			return -1;
+	} else {
+		return -1;
+	}
+	mp_decode_uint(&data);
+	if (data == end)
+		return -1;
+	if (mp_typeof(*data) != MP_ARRAY)
+		return -1;
+	return mp_check(&data, end) != 0 || data != end;
+}

test/unit/mp_tuple.c
@@ -1,0 +1,296 @@
+#include "box/mp_tuple.h"
+#include "box/tuple.h"
+#include "box/tuple_format_map.h"
+
+#include "core/fiber.h"
+#include "core/memory.h"
+
+#include "mpstream/mpstream.h"
+
+#include "msgpuck/msgpuck.h"
+
+#define UNIT_TAP_COMPATIBLE 1
+#include "unit.h"
+
+static void
+mpstream_error(void *is_err)
+{
+	*(bool *)is_err = true;
+}
+
+static int
+test_mp_sizeof_tuple(void)
+{
+	plan(1);
+	header();
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	is(mp_sizeof_tuple(tuple),
+	   mp_sizeof_ext(mp_sizeof_uint(tuple->format_id) + tuple_bsize(tuple)),
+	   "sizeof tuple works correctly");
+	tuple_delete(tuple);
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_mp_encode_tuple(void)
+{
+	plan(1);
+	header();
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	char *expected = mp_encode_tuple(buf, tuple);
+	size_t encoded_len = expected - buf;
+	size_t expected_len =
+		mp_sizeof_uint(tuple->format_id) + tuple_bsize(tuple);
+	tuple_delete(tuple);
+	char *w = expected;
+	w = mp_encode_extl(w, MP_TUPLE, expected_len);
+	w = mp_encode_uint(w, tuple->format_id);
+	mp_encode_array(w, 0);
+	is(memcmp(buf, expected, MIN(encoded_len, expected_len)), 0,
+	   "MP_TUPLE encoding works correctly");
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_tuple_to_mpstream_as_ext(void)
+{
+	plan(1);
+	header();
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	struct mpstream stream;
+	struct region *region = &fiber()->gc;
+	size_t region_svp = region_used(region);
+	bool is_err = false;
+	mpstream_init(&stream, region, region_reserve_cb, region_alloc_cb,
+		      mpstream_error, &is_err);
+	tuple_to_mpstream_as_ext(tuple, &stream);
+	mpstream_flush(&stream);
+	fail_if(is_err);
+	size_t data_len = region_used(region) - region_svp;
+	const char *data = xregion_join(region, data_len);
+	char *expected = mp_encode_tuple(buf, tuple);
+	tuple_delete(tuple);
+	size_t expected_len = expected - buf;
+	is(memcmp(data, expected, MIN(data_len, expected_len)), 0,
+	   "MP_TUPLE encoding to MsgPack stream works correctly");
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_mp_validate_tuple(void)
+{
+	plan(8);
+	header();
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	char *w = mp_encode_tuple(buf, tuple);
+	tuple_delete(tuple);
+	size_t encoded_len = w - buf;
+	int8_t unused;
+	const char *r = buf;
+	uint32_t ext_len = mp_decode_extl(&r, &unused);
+	is(mp_validate_tuple(r, ext_len), 0,
+	   "MP_TUPLE validation works correctly for valid tuple");
+
+	encoded_len = mp_sizeof_nil();
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	mp_encode_nil(w);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(UINT32_MAX) - 1;
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	mp_encode_uint(w, UINT32_MAX);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(w, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(777);
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	mp_encode_uint(w, 777);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(777) + mp_sizeof_nil();
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	w = mp_encode_uint(w, 777);
+	mp_encode_nil(w);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(777) + mp_sizeof_nil();
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	w = mp_encode_uint(w, 777);
+	mp_encode_nil(w);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(777) + mp_sizeof_array(1);
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	w = mp_encode_uint(w, 777);
+	w = mp_encode_array(w, 1);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(777) + mp_sizeof_array(1);
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	w = mp_encode_uint(w, 777);
+	w = mp_encode_array(w, 1);
+	mp_encode_nil(w);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_tuple_unpack(void)
+{
+	plan(3);
+	header();
+
+	struct tuple_format_map format_map;
+	tuple_format_map_create_empty(&format_map);
+	tuple_format_map_add_format(&format_map, tuple_format_runtime->id);
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	mp_encode_tuple(buf, tuple);
+	tuple_delete(tuple);
+	struct tuple *decoded;
+	const char *r = buf;
+	int8_t unused;
+	mp_decode_extl(&r, &unused);
+	decoded = tuple_unpack(&r, &format_map);
+	isnt(decoded, NULL, "valid MP_TUPLE is unpacked correctly");
+	is(decoded->format_id, tuple_format_runtime->id,
+	   "valid MP_TUPLE is unpacked correctly");
+	tuple_delete(decoded);
+	tuple_format_map_destroy(&format_map);
+
+	tuple_format_map_create_empty(&format_map);
+	r = buf;
+	mp_decode_extl(&r, &unused);
+	is(tuple_unpack(&r, &format_map), NULL,
+	   "MP_TUPLE with invalid format is unpacked correctly");
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_mp_decode_tuple(void)
+{
+	plan(4);
+	header();
+
+	struct tuple_format_map format_map;
+	tuple_format_map_create_empty(&format_map);
+	tuple_format_map_add_format(&format_map, tuple_format_runtime->id);
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	mp_encode_tuple(buf, tuple);
+	tuple_delete(tuple);
+	struct tuple *decoded;
+	const char *r = buf;
+	decoded = mp_decode_tuple(&r, &format_map);
+	isnt(decoded, NULL, "valid MP_TUPLE is decoded correctly");
+	is(decoded->format_id, tuple_format_runtime->id,
+	   "valid MP_TUPLE is decoded correctly");
+	tuple_delete(decoded);
+
+	char *w = buf;
+	mp_encode_nil(w);
+	r = buf;
+	is(mp_decode_tuple(&r, &format_map), NULL,
+	   "invalid MP_TUPLE is decoded correctly");
+
+	w = buf;
+	w = mp_encode_extl(w, mp_extension_type_MAX, 1);
+	r = buf;
+	is(mp_decode_tuple(&r, &format_map), NULL,
+	   "invalid MP_TUPLE is decoded correctly");
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_mp_tuple(void)
+{
+	plan(6);
+	header();
+
+	test_mp_sizeof_tuple();
+	test_mp_encode_tuple();
+	test_tuple_to_mpstream_as_ext();
+	test_mp_validate_tuple();
+	test_tuple_unpack();
+	test_mp_decode_tuple();
+
+	footer();
+	return check_plan();
+}
+
+static uint32_t
+field_name_hash_impl(const char *str, uint32_t len)
+{
+	return str[0] + len;
+}
+
+int
+main(void)
+{
+	memory_init();
+	fiber_init(fiber_c_invoke);
+	tuple_init(field_name_hash_impl);
+
+	int rc = test_mp_tuple();
+
+	tuple_free();
+	fiber_free();
+	memory_free();
+	return rc;
+}

tsafin/tarantool-dsql

src/box/mp_tuple.c
@@ -1,0 +1,117 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright 2010-2023, Tarantool AUTHORS, please see AUTHORS file.
+ */
+
+#include "box/mp_tuple.h"
+#include "box/tuple.h"
+#include "box/tuple_format_map.h"
+
+#include "core/mp_extension_types.h"
+
+#include "mpstream/mpstream.h"
+
+/** Length of packed tuple. */
+static uint32_t
+tuple_len(struct tuple *tuple)
+{
+	return mp_sizeof_uint(tuple->format_id) + tuple_bsize(tuple);
+}
+
+uint32_t
+mp_sizeof_tuple(struct tuple *tuple)
+{
+	return mp_sizeof_ext(tuple_len(tuple));
+}
+
+/** Pack a tuple value to a buffer. */
+static char *
+tuple_pack(char *data, struct tuple *tuple)
+{
+	data = mp_encode_uint(data, tuple->format_id);
+	uint32_t bsize = tuple_bsize(tuple);
+	memcpy(data, tuple_data(tuple), bsize);
+	return data + bsize;
+}
+
+struct tuple *
+mp_decode_tuple(const char **data, struct tuple_format_map *format_map)
+{
+	if (mp_typeof(**data) != MP_EXT)
+		return NULL;
+	int8_t type;
+	const char *svp = *data;
+	mp_decode_extl(data, &type);
+	if (type != MP_TUPLE)
+		goto error;
+	struct tuple *tuple = tuple_unpack(data, format_map);
+	if (tuple == NULL)
+		goto error;
+	return tuple;
+error:
+	*data = svp;
+	return NULL;
+}
+
+struct tuple *
+tuple_unpack(const char **data, struct tuple_format_map *format_map)
+{
+	uint32_t format_id = mp_decode_uint(data);
+	const char *tuple_data = *data;
+	mp_next(data);
+	struct tuple_format *format =
+		tuple_format_map_find(format_map, format_id);
+	if (format == NULL)
+		return NULL;
+	return tuple_new(format, tuple_data, *data);
+}
+
+struct tuple *
+tuple_unpack_without_format(const char **data)
+{
+	/* Ignore the format identfier.  */
+	mp_decode_uint(data);
+	const char *tuple_data = *data;
+	mp_next(data);
+	return tuple_new(tuple_format_runtime, tuple_data, *data);
+}
+
+char *
+mp_encode_tuple(char *data, struct tuple *tuple)
+{
+	data = mp_encode_extl(data, MP_TUPLE, tuple_len(tuple));
+	return tuple_pack(data, tuple);
+}
+
+void
+tuple_to_mpstream_as_ext(struct tuple *tuple, struct mpstream *stream)
+{
+	uint32_t tuple_sz = mp_sizeof_tuple(tuple);
+	char *data = mpstream_reserve(stream, tuple_sz);
+	if (data == NULL)
+		return;
+	char *pos = mp_encode_tuple(data, tuple);
+	mpstream_advance(stream, pos - data);
+}
+
+int
+mp_validate_tuple(const char *data, uint32_t len)
+{
+	/*
+	 * MsgPack extensions have length greater or equal than 1 by
+	 * specification.
+	 */
+	assert(len > 0);
+	const char *end = data + len;
+	enum mp_type type = mp_typeof(*data);
+	if (type != MP_UINT || mp_check_uint(data, end) > 0)
+		return -1;
+
+	mp_next(&data);
+	if (data == end)
+		return -1;
+	if (mp_typeof(*data) != MP_ARRAY)
+		return -1;
+	return mp_check(&data, end) != 0 || data != end;
+}

test/unit/mp_tuple.c
@@ -1,0 +1,310 @@
+#include "box/mp_box_ctx.h"
+#include "box/mp_tuple.h"
+#include "box/tuple.h"
+#include "box/tuple_format_map.h"
+
+#include "core/fiber.h"
+#include "core/memory.h"
+
+#include "mpstream/mpstream.h"
+
+#include "msgpuck/msgpuck.h"
+
+#define UNIT_TAP_COMPATIBLE 1
+#include "unit.h"
+
+static void
+mpstream_error(void *is_err)
+{
+	*(bool *)is_err = true;
+}
+
+static int
+test_mp_sizeof_tuple(void)
+{
+	plan(1);
+	header();
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	is(mp_sizeof_tuple(tuple),
+	   mp_sizeof_ext(mp_sizeof_uint(tuple->format_id) + tuple_bsize(tuple)),
+	   "sizeof tuple works correctly");
+	tuple_delete(tuple);
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_mp_encode_tuple(void)
+{
+	plan(2);
+	header();
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	char *expected = mp_encode_tuple(buf, tuple);
+	size_t encoded_len = expected - buf;
+	size_t ext_len =
+		mp_sizeof_uint(tuple->format_id) + tuple_bsize(tuple);
+	size_t expected_len = mp_sizeof_ext(ext_len);
+	is(encoded_len, expected_len);
+	char *w = expected;
+	w = mp_encode_extl(w, MP_TUPLE, ext_len);
+	w = mp_encode_uint(w, tuple->format_id);
+	mp_encode_array(w, 0);
+	is(memcmp(buf, expected, encoded_len), 0,
+	   "MP_TUPLE encoding works correctly");
+	tuple_delete(tuple);
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_tuple_to_mpstream_as_ext(void)
+{
+	plan(2);
+	header();
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	struct mpstream stream;
+	struct region *region = &fiber()->gc;
+	size_t region_svp = region_used(region);
+	bool is_err = false;
+	mpstream_init(&stream, region, region_reserve_cb, region_alloc_cb,
+		      mpstream_error, &is_err);
+	tuple_to_mpstream_as_ext(tuple, &stream);
+	mpstream_flush(&stream);
+	fail_if(is_err);
+	size_t data_len = region_used(region) - region_svp;
+	const char *data = xregion_join(region, data_len);
+	char *expected = mp_encode_tuple(buf, tuple);
+	tuple_delete(tuple);
+	size_t expected_len = expected - buf;
+	is(data_len, expected_len);
+	is(memcmp(data, buf, data_len), 0,
+	   "MP_TUPLE encoding to MsgPack stream works correctly");
+	region_truncate(region, region_svp);
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_mp_validate_tuple(void)
+{
+	plan(8);
+	header();
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	char *w = mp_encode_tuple(buf, tuple);
+	tuple_delete(tuple);
+	size_t encoded_len = w - buf;
+	int8_t unused;
+	const char *r = buf;
+	uint32_t ext_len = mp_decode_extl(&r, &unused);
+	is(mp_validate_tuple(r, ext_len), 0,
+	   "MP_TUPLE validation works correctly for valid tuple");
+
+	encoded_len = mp_sizeof_nil();
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	mp_encode_nil(w);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(UINT32_MAX) - 1;
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	mp_encode_uint(w, UINT32_MAX);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(w, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(777);
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	mp_encode_uint(w, 777);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(777) + mp_sizeof_nil();
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	w = mp_encode_uint(w, 777);
+	mp_encode_nil(w);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(777) + mp_sizeof_nil();
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	w = mp_encode_uint(w, 777);
+	mp_encode_nil(w);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(777) + mp_sizeof_array(1);
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	w = mp_encode_uint(w, 777);
+	w = mp_encode_array(w, 1);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	encoded_len = mp_sizeof_uint(777) + mp_sizeof_array(1);
+	w = buf;
+	w = mp_encode_extl(w, MP_TUPLE, encoded_len);
+	w = mp_encode_uint(w, 777);
+	w = mp_encode_array(w, 1);
+	mp_encode_nil(w);
+	r = buf;
+	ext_len = mp_decode_extl(&r, &unused);
+	isnt(mp_validate_tuple(r, ext_len), 0,
+	     "MP_TUPLE validation works correctly for invalid tuple");
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_tuple_unpack(void)
+{
+	plan(5);
+	header();
+
+	struct tuple_format_map format_map;
+	tuple_format_map_create_empty(&format_map);
+	tuple_format_map_add_format(&format_map, tuple_format_runtime->id);
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	mp_encode_tuple(buf, tuple);
+	tuple_delete(tuple);
+	struct tuple *decoded;
+	const char *r = buf;
+	int8_t unused;
+	mp_decode_extl(&r, &unused);
+	decoded = tuple_unpack(&r, &format_map);
+	isnt(decoded, NULL, "valid MP_TUPLE is unpacked correctly");
+	is(decoded->format_id, tuple_format_runtime->id,
+	   "valid MP_TUPLE is unpacked correctly");
+	tuple_delete(decoded);
+	tuple_format_map_destroy(&format_map);
+
+	tuple_format_map_create_empty(&format_map);
+	r = buf;
+	mp_decode_extl(&r, &unused);
+	is(tuple_unpack(&r, &format_map), NULL,
+	   "MP_TUPLE with invalid format is unpacked correctly");
+
+	r = buf;
+	mp_decode_extl(&r, &unused);
+	decoded = tuple_unpack_without_format(&r);
+	isnt(decoded, NULL,
+	     "valid MP_TUPLE is unpacked without format correctly");
+	is(decoded->format_id, tuple_format_runtime->id,
+	   "valid MP_TUPLE is unpacked without format correctly");
+	tuple_delete(decoded);
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_mp_decode_tuple(void)
+{
+	plan(4);
+	header();
+
+	struct tuple_format_map format_map;
+	tuple_format_map_create_empty(&format_map);
+	tuple_format_map_add_format(&format_map, tuple_format_runtime->id);
+
+	char buf[1024];
+	size_t size = mp_format(buf, lengthof(buf), "[]");
+	struct tuple *tuple = tuple_new(tuple_format_runtime, buf, buf + size);
+	mp_encode_tuple(buf, tuple);
+	tuple_delete(tuple);
+	struct tuple *decoded;
+	const char *r = buf;
+	decoded = mp_decode_tuple(&r, &format_map);
+	isnt(decoded, NULL, "valid MP_TUPLE is decoded correctly");
+	is(decoded->format_id, tuple_format_runtime->id,
+	   "valid MP_TUPLE is decoded correctly");
+	tuple_delete(decoded);
+
+	char *w = buf;
+	mp_encode_nil(w);
+	r = buf;
+	is(mp_decode_tuple(&r, &format_map), NULL,
+	   "invalid MP_TUPLE is decoded correctly");
+
+	w = buf;
+	mp_encode_extl(w, mp_extension_type_MAX, 1);
+	r = buf;
+	is(mp_decode_tuple(&r, &format_map), NULL,
+	   "invalid MP_TUPLE is decoded correctly");
+
+	footer();
+	return check_plan();
+}
+
+static int
+test_mp_tuple(void)
+{
+	plan(6);
+	header();
+
+	test_mp_sizeof_tuple();
+	test_mp_encode_tuple();
+	test_tuple_to_mpstream_as_ext();
+	test_mp_validate_tuple();
+	test_tuple_unpack();
+	test_mp_decode_tuple();
+
+	footer();
+	return check_plan();
+}
+
+static uint32_t
+field_name_hash_impl(const char *str, uint32_t len)
+{
+	return str[0] + len;
+}
+
+int
+main(void)
+{
+	memory_init();
+	fiber_init(fiber_c_invoke);
+	tuple_init(field_name_hash_impl);
+
+	int rc = test_mp_tuple();
+
+	tuple_free();
+	fiber_free();
+	memory_free();
+	return rc;
+}