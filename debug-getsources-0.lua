8b33d67db3054e3d66309fea17c5adb59a4bf40d
Timur Safin <timur.safin@gmail.com>
2026-04-20 01:24:10 +0300

    cleanup: remove vibecoding artefacts

--- debug-getsources-0.lua
+++
@@ -1,6 +0,0 @@
-local tarantool = (true)
-local luaGetSources = tarantool and tarantool.debug and 
-                tarantool.debug.getsources or
-                function(filepath) return nil end
-
-return luaGetSources
